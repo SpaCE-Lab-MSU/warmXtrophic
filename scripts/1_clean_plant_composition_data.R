7b35d2d7e701319e3caebf03a260275b61b5b38a
ninalany <lanynina@msu.edu>
2019-04-16 14:54:51 -0400

    update USDA species codes

--- scripts/1_clean_plant_composition_data.R
+++ scripts/1_clean_plant_composition_data.R
@@ -96,6 +96,7 @@ rm(list = c('dat', 'data'))
 ###2016
 data <- gs_title("kbs_plant_comp_2016")
 dat <- as.data.frame(gs_read(data))
+#dat <- read.csv("data/L0/KBS/2016/kbs_plant_comp_2016.csv", stringsAsFactors=F)
 dat$Site <- "kbs"
 unique(dat$Date)
 dat$Date <- as.Date(dat$Date, format = "%m/%d/%Y")
@@ -126,6 +127,10 @@ drive_upload("kbs_plant_comp_2016.csv",
 drive_share("kbs_plant_comp_2016", role = "write", type = "anyone") #change permissions to 'anyone with the link can edit'
 #remove local file
 file.remove("kbs_plant_comp_2016.csv")
+
+#alternately:
+#write.csv(dat, "data/L1/plant_composition/kbs_plant_comp_2016.csv", row.names=F)
+
 #remove data from workspace
 rm(list = c('dat', 'data'))
 
@@ -133,6 +138,7 @@ rm(list = c('dat', 'data'))
 ###2017
 data <- gs_title("kbs_plant_comp_2017")
 dat <- as.data.frame(gs_read(data))
+#dat <- read.csv("data/L0/KBS/2017/kbs_plant_comp_2017.csv", stringsAsFactors=F)
 unique(dat$Site)
 unique(dat$Date)  ## Is 3/2/2017 reasonable?
 dat <- dat[dat$Date != "5/10/2016",] #remove random single row from 2016
@@ -146,7 +152,7 @@ unique(dat$Species)
 dat$Species[dat$Species=="Bown"] <- "Brown" #typo
 dat$Species[dat$Species=="Bare Ground"] <- "Bare_Ground"
 dat$Species[dat$Species=="Hipr"] <- "Hica" #Hieracium pratense is a synonym for Hieracium caespitosum
-#dat$Species[dat$Species=="Ramu"] <- "Romu" #RAMU is not in the USDA plants databse. ROMU is Rosa multiflora. 
+#dat$Species[dat$Species=="Ramu"] <- "Romu" #RAMU is not in the USDA plants databse. ROMU is Rosa multiflora. This change was in Phoebe's notes without explanation.
 #compare species codes with taxon lookup table
 setdiff(unique(dat$Species), unique(taxa$code))
 #[1]"Ramu" "Brin"
@@ -170,6 +176,10 @@ drive_upload("kbs_plant_comp_2017.csv",
 drive_share("kbs_plant_comp_2017", role = "write", type = "anyone") #change permissions to 'anyone with the link can edit'
 #remove local file
 file.remove("kbs_plant_comp_2017.csv")
+
+#alternately:
+#write.csv(dat, "data/L1/plant_composition/kbs_plant_comp_2017.csv", row.names=F)
+
 #remove data from workspace
 rm(list = c('dat', 'data'))
 
@@ -185,6 +195,7 @@ rm(list = c('dat', 'data'))
 #2015
 data <- gs_title("umbs_plant_comp_2015")
 dat <- as.data.frame(gs_read(data))
+#dat <- read.csv("data/L0/UMBS/2015/umbs_plant_comp_2015.csv", stringsAsFactors=F)
 dat$Site <- tolower(dat$Site)
 unique(dat$Site)
 unique(dat$Date)  
@@ -194,6 +205,7 @@ unique(dat$Plot)
 #compare plot codes with plot lookup table:
 setdiff(unique(dat$Plot), unique(plots$plot))
 unique(dat$Species)
+dat$Species[dat$Species=="Piau"] <- "Hiau" #USDA accepted name for Pilosella aurantica is Hieracium auranticum
 setdiff(unique(dat$Species), unique(taxa$code))
 #in 2015 only, percent cover was measured separately on four quadrants of each quadrat(plot). Aggregate to get percent cover of each quadrat (plot):
 dat$Cover <- dat$Cover * .25
@@ -218,12 +230,17 @@ drive_upload("umbs_plant_comp_2015.csv",
 drive_share("umbs_plant_comp_2015", role = "write", type = "anyone") #change permissions to 'anyone with the link can edit'
 #remove local file
 file.remove("umbs_plant_comp_2015.csv")
+
+#alternately:
+#write.csv(dat, "data/L1/plant_composition/umbs_plant_comp_2015.csv", row.names=F)
+
 #remove data from workspace
 rm(list = c('dat', 'data'))
 
 #2016
 data <- gs_title("umbs_plant_comp_2016")
 dat <- as.data.frame(gs_read(data))
+#dat <- read.csv("data/L0/UMBS/2016/umbs_plant_comp_2016.csv", stringsAsFactors=F)
 dat$Site <- 'umbs'
 unique(dat$Date)  
 #convert date to common format:
@@ -234,6 +251,7 @@ setdiff(unique(dat$Plot), unique(plots$plot))
 unique(dat$Species)
 dat$Species[dat$Species=="Bareground"] <- "Bare_Ground"
 dat$Species[dat$Species=="Anspp"] <- "Ansp" #probably a typo
+dat$Species[dat$Species=="Piau"] <- "Hiau" #USDA accepted name for Pilosella aurantica is Hieracium auranticum
 setdiff(unique(dat$Species), unique(taxa$code))
 colnames(dat)[which(names(dat) == "Percent_Cover")] <- "Cover"
 hist(dat$Cover)
@@ -256,6 +274,10 @@ drive_upload("umbs_plant_comp_2016.csv",
 drive_share("umbs_plant_comp_2016", role = "write", type = "anyone") #change permissions to 'anyone with the link can edit'
 #remove local file
 file.remove("umbs_plant_comp_2016.csv")
+
+#alternately:
+#write.csv(dat, "data/L1/plant_composition/umbs_plant_comp_2016.csv", row.names=F)
+
 #remove data from workspace
 rm(list = c('dat', 'data'))
 
@@ -263,6 +285,7 @@ rm(list = c('dat', 'data'))
 #2017
 data <- gs_title("umbs_plant_comp_2017")
 dat <- as.data.frame(gs_read(data))
+#dat <- read.csv("data/L0/UMBS/2017/umbs_plant_comp_2017.csv", stringsAsFactors=F)
 unique(dat$Site)
 dat <- dat[dat$Date != "5/10/2016",] #remove random single row from 2016
 unique(dat$Date)  
@@ -277,6 +300,7 @@ dat$Species[dat$Species=="Bare Groud"] <- "Bare_Ground"
 dat$Species[dat$Species=="Bare"] <- "Bare_Ground"
 dat$Species[dat$Species=="Unknown 5"] <- "Unknown"
 dat$Species[dat$Species=="Vesp"] <- "Vear" #from Phoebe's cleaning script
+dat$Species[dat$Species=="Piau"] <- "Hiau" #USDA accepted name for Pilosella aurantica is Hieracium auranticum
 setdiff(unique(dat$Species), unique(taxa$code))
 hist(dat$Cover)
 summary(dat$Cover)
@@ -298,12 +322,17 @@ drive_upload("umbs_plant_comp_2017.csv",
 drive_share("umbs_plant_comp_2017", role = "write", type = "anyone") #change permissions to 'anyone with the link can edit'
 #remove local file
 file.remove("umbs_plant_comp_2017.csv")
+
+#alternately:
+#write.csv(dat, "data/L1/plant_composition/umbs_plant_comp_2017.csv", row.names=F)
+
 #remove data from workspace
 rm(list = c('dat', 'data'))
 
 #2018
 data <- gs_title("umbs_plant_comp_2018")
 dat <- as.data.frame(gs_read(data))
+#dat <- read.csv("data/L0/UMBS/2018/umbs_plant_comp_2018.csv", stringsAsFactors=F)
 unique(dat$Site)
 unique(dat$Date)  
 #convert date to common format:
@@ -314,18 +343,19 @@ setdiff(unique(dat$Plot), unique(plots$plot))
 unique(dat$Species)
 dat$Species[dat$Species=="UNKNOWN1"] <- "Unknown"
 dat$Species[dat$Species=="UKNOWN_sp"] <- "Unknown"
-dat$Species[dat$Species=="UNKNOWN_GRASS3"] <- "Unknown"
-dat$Species[dat$Species=="UKNOWN_Grass"] <- "Unknown"
+dat$Species[dat$Species=="UNKNOWN_GRASS3"] <- "Posp"
+dat$Species[dat$Species=="UKNOWN_Grass"] <- "Posp"
 dat$Species[dat$Species=="Unknown_shrub"] <- "Unknown"
 dat$Species[dat$Species=="UNKNOWN_PLANT1"] <- "Unknown"
 dat$Species[dat$Species=="WILD_RASP?"] <- "Unknown"
-dat$Species[dat$Species=="UNKNOWN_GRASS2"] <- "Unknown"
+dat$Species[dat$Species=="UNKNOWN_GRASS2"] <- "Posp"
 dat$Species[dat$Species=="UNKNOWN 5"] <- "Unknown"
 dat$Species[dat$Species=="UNKNOWN_7"] <- "Unknown"
-dat$Species[dat$Species=="UNKNOWN_GRASS"] <- "Unknown"
-dat$Species[dat$Species=="Unknown_grass"] <- "Unknown"
-dat$Species[dat$Species=="unknown_grass"] <- "Unknown"
+dat$Species[dat$Species=="UNKNOWN_GRASS"] <- "Posp"
+dat$Species[dat$Species=="Unknown_grass"] <- "Posp"
+dat$Species[dat$Species=="unknown_grass"] <- "Posp"
 dat$Species[dat$Species=="Unknown."] <- "Unknown"
+dat$Species[dat$Species=="Piau"] <- "Hiau" #USDA accepted name for Pilosella aurantica is Hieracium auranticum
 setdiff(unique(dat$Species), unique(taxa$code))
 #[1] "Prse" "Amla" "Ptsp" "Apan"
 hist(dat$Cover)
@@ -339,7 +369,7 @@ str(dat)
 googledrive::drive_ls("~/warmXtrophic/data/L1/plant_composition")
 # temp write local
 readr::write_csv(dat, "umbs_plant_comp_2018.csv")
-#drive_rm("umbs_plant_comp_2018.csv") #delete old version
+drive_rm("umbs_plant_comp_2018.csv") #delete old version
 drive_upload("umbs_plant_comp_2018.csv", 
              path = "~/warmXtrophic/data/L1/plant_composition", 
              name = "umbs_plant_comp_2018.csv", 
@@ -348,5 +378,8 @@ drive_upload("umbs_plant_comp_2018.csv",
 drive_share("umbs_plant_comp_2018", role = "write", type = "anyone") #change permissions to 'anyone with the link can edit'
 #remove local file
 file.remove("umbs_plant_comp_2018.csv")
+#Alternately, write locally.
+#dat <- read.csv("data/L0/UMBS/2018/umbs_plant_comp_2018.csv", stringsAsFactors=F)
+
 #remove data from workspace
 rm(list = c('dat', 'data'))
