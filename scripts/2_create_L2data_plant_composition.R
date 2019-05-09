e9c7445645f314f0b0d487661e674bddc9fb6895
ninalany <lanynina@msu.edu>
2019-05-09 11:14:47 -0400

    create L2 data with checks

--- scripts/2_create_L2data_plant_composition.R
+++ scripts/2_create_L2data_plant_composition.R
@@ -26,7 +26,34 @@ L1_data_dir <- googledrive::drive_ls("~/warmXtrophic/data/L1/plant_composition")
 L2_data_dir <- googledrive::drive_ls("~/warmXtrophic/data/L2")
 
 #if you will be using Google Drive File Stream or Back Up and Sync (not the 'googledrive' and 'googlesheets' packages), set your working directory to the warmXtrophic folder on the shared Google Drive.
-#setwd("~/Google Drive File Stream/My Drive/warmXtrophic")
+setwd("~/Google Drive File Stream/My Drive/warmXtrophic")
 #Check to make sure working directory is correct
 if(basename(getwd())!="warmXtrophic"){cat("Plz change your working directory. It should be 'warmXtrophic'")}
 
+#load the plot and taxon tables for reference
+taxa <- read.csv("~/Google Drive File Stream/My Drive/warmXtrophic/data/L2/taxon.csv", stringsAsFactors=F)
+plots <- read.csv("~/Google Drive File Stream/My Drive/warmXtrophic/data/L2/plot.csv", stringsAsFactors=F)
+events <- read.csv("~/Google Drive File Stream/My Drive/warmXtrophic/data/L2/event.csv", stringsAsFactors=F)
+
+
+######################
+# read in L1 data files and rbind them together
+file_list <- list.files("data/L1/plant_composition", pattern = ".csv")
+setwd("~/Google Drive File Stream/My Drive/warmXtrophic/data/L1/plant_composition")
+newdat <- do.call(rbind,lapply(file_list, read.csv, header=T, stringsAsFactors=F))
+
+#some quick final checks:
+unique(newdat$Site)
+unique(newdat$Year)
+setdiff(unique(newdat$Date), unique(events$Date)) #make sure it is zero
+setdiff(unique(newdat$Plot), unique(plots$plot)) #make sure it is zero
+setdiff(unique(newdat$Species), unique(taxa$code)) #make sure it is zero
+hist(newdat$Cover) #make sure it ranges from 0-100
+
+#convert to observation-variable-value form:
+newdat$observation_id <- paste(newdat$Site,newdat$Date,newdat$Plot,newdat$Species, sep = "_")
+newdat$observation_id[1:25]
+colnames(newdat)[colnames(newdat)=="Species"] <- "variable_name"
+colnames(newdat)[colnames(newdat)=="Cover"] <- "value"
+
+write.csv(newdat, file = "~/Google Drive File Stream/My Drive/warmXtrophic/data/L2/observation_species.csv", row.names = F)
