ca5ae54e5a6a157f8f300878295c4bf6583e6c55
ninalany <lanynina@msu.edu>
2019-05-30 10:41:42 -0400

    add function to plot phenology time series

--- scripts/functions.R
+++ scripts/functions.R
@@ -213,3 +213,28 @@ ggplot(data=cuml.taxa.by.site, aes(x = year, y = no.taxa)) +
   theme(axis.title = element_text(size=12), axis.text = element_text(size=12), legend.position = "right") #
 
 }
+
+############################
+# PHENOLOGY FUNCTIONS
+############################
+
+
+#plot time series of percent cover for a given species within a given year
+
+plot_phenology_ts <- function (dat, species, year){
+	newdat <- merge(dat, plots, by.x = "Plot", by.y = "plot")
+	temp1 <- subset(newdat, Species == species & Year == year)
+	plot(temp1$Julian_day, temp1$Cover, type = "n", xlim = c(min(temp1$Julian_day), max(temp1$Julian_day)), ylim = c(0,max(temp1$Cover)), xlab = "Julian day", ylab = "Percent cover", main = paste(species," - ", year)) #this makes a blank plot
+
+plot_id <- as.vector(unique(temp1$Plot))
+for(p in 1:length(plot_id)){ #fil in the plot with a line for each plot
+	temp2 <- subset(temp1, Plot == plot_id[p])
+	temp2 <- temp2[order(temp2$Julian_day),] #make sure dataframe is ordered by date
+	color_key <- ifelse(unique(temp2$state)=="warmed", "red", "blue") #color codeaccording to treatment
+	lines(temp2$Julian_day, temp2$Cover, col = color_key, type = "o")
+	}
+	legend("topleft", col = c("blue", "red"), legend = c("ambient", "warmed"), pch = 1, lty=1) #add a legend only once	
+}
+
+
+
