b23285a069f670053ee5d64a4ef968b4cd1717ea
Moriah Young <moriahyoung@Moriahs-MacBook-Air.local>
2020-11-30 11:41:28 -0500

    updated to work with google filestream and added code to save a new .csv file

--- moriah/ANPP_biomass.R
+++ moriah/ANPP_biomass.R
@@ -11,21 +11,18 @@ rm(list = ls())
 
 # Load packages
 library(tidyverse)
-library(googlesheets4)
-library(gsheet)
-library(googledrive)
 
 # Set working directory to Google Drive
 setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
 
 # Read in data
-biomass <- read_sheet("https://docs.google.com/spreadsheets/d/1SNjYiEhtNelFB58c-cfGUNGWO7kreP05_lKXdD3-RLM/edit#gid=0")
-
-#biomass <- read.csv("L0/KBS/2020/kbs_ancillary_ANPP_2020.csv")
-#taxa <- read.csv("/Users/moriahyoung/Desktop/taxon_list.csv")
+biomass <- read.csv("L0/KBS/2020/kbs_ancillary_biomass_2020.csv")
+biomass1 <- as.data.frame(biomass) # make into data frame
+# Make sure that you create a new .csv file of the taxon_list.xlsx file IN the google shared folder
+# to ensure that you have the most up to date species list
+taxa <- read.csv("L2/taxon_uptodate.csv")
 
 # Clean biomass data 
-biomass1 <- as.data.frame(biomass)
 biomass2 <- biomass %>% # get ride of unneeded columns
         select(-dry_weight_g, -bag, -bag_size, -weight, -n_bags, -bag_weight, -bag_code, -notes)
 
@@ -35,6 +32,9 @@ names(biomass2)[names(biomass2)=="final_biomass_g"] <- "weight_g"
 biomass.final <- with(biomass2, biomass2[order(plot, weight_g),])
 View(biomass.final)
 
+# Change date from factor to date
+biomass.final$date <- as.Date(biomass.final$date,format="%m/%d/%Y")
+
 table(unique(biomass.final$plot)) # Are there 19 plots?
 unique(biomass.final$species) # Are all the species unique?
 
@@ -42,3 +42,6 @@ unique(biomass.final$species) # Are all the species unique?
 # with the taxon lookup table (spp)
 setdiff(unique(biomass.final$species), unique(taxa$code))
 
+# Save a .csv file with the cleaned biomass
+write.csv(biomass.final, file = "L0/KBS/2020/kbs_biomass_final.csv", row.names = FALSE)
+#this will save a cleaned version of the KBS ANPP Biomass 2020 file in the shared folder of the warmXtrophic project
