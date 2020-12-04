0dea99df0be3eeb17eca5198067af6cb4e479a5e
Kara Dobson <dobsonk2@msu.edu>
2020-12-04 13:44:30 -0500

    created plant comp scripts

---
+++ kara/plant_response_scripts/clean_plant_comp_L0.R
@@ -0,0 +1,20 @@
+# TITLE:          Plant composition cleanup (2018 & 2020)
+# AUTHORS:        Kara Dobson
+# COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Moriah Young
+# DATA INPUT:     Data imported as csv files from shared Google drive L0 folder
+# DATA OUTPUT:    Clean csv files are uploaded to the L1 Google drive folder
+# PROJECT:        warmXtrophic
+# DATE:           November, 2020
+
+# Clear all existing data
+rm(list=ls())
+
+#Load packages
+library(tidyverse)
+
+# Set working directory to Google Drive
+# **** Update with the path to your Google drive on your computer
+setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
+
+# Read in data
+
--- kara/plant_response_scripts/merge_plant_comp_L1.R
+++ kara/plant_response_scripts/merge_plant_comp_L1.R
@@ -1,4 +1,4 @@
-# TITLE:          Solidago canadensis data
+# TITLE:          Plant composition: merging data
 # AUTHORS:        Kara Dobson
 # COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Moriah Young
 # DATA INPUT:     Data imported as csv files from shared Google drive L0 folder
@@ -15,3 +15,9 @@ library(tidyverse)
 # Set working directory to Google Drive
 # **** Update with the path to your Google drive on your computer
 setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
+
+# Read in data
+kbs_2015 <- read.csv("L1/plant_composition/kbs_plant_comp_2015.csv")
+kbs_2016 <- read.csv("L1/plant_composition/kbs_plant_comp_2016.csv")
+kbs_2017 <- read.csv("L1/plant_composition/kbs_plant_comp_2017.csv")
+kbs_2019 <- read.csv("L1/plant_composition/kbs_plant_comp_2019.csv")
