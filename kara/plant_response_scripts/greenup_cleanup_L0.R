54b89f656312e4fe6ca5bcb2c14e8f20a0752d6b
Kara Dobson <dobsonk2@msu.edu>
2020-12-04 12:22:06 -0500

    updating plant response scripts

---
+++ kara/plant_response_scripts/greenup_cleanup_L0.R
@@ -0,0 +1,17 @@
+# TITLE:          Solidago canadensis data
+# AUTHORS:        Kara Dobson
+# COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Moriah Young
+# DATA INPUT:     Data imported as csv files from shared Google drive L0 folder
+# DATA OUTPUT:    
+# PROJECT:        warmXtrophic
+# DATE:           November, 2020
+
+# Clear all existing data
+rm(list=ls())
+
+#Load packages
+library(tidyverse)
+
+# Set working directory to Google Drive
+# **** Update with the path to your Google drive on your computer
+setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
