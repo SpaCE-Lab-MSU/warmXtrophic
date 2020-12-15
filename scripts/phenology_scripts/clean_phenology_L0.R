874b826a1e409dc9c70e1e75090df2c191b8abc5
Moriah Young <moriahyoung@Moriahs-MacBook-Air.local>
2020-12-15 12:31:19 -0500

    fixing species codes and removing file from folder

--- scripts/phenology_scripts/clean_phenology_L0.R
+++ scripts/phenology_scripts/clean_phenology_L0.R
@@ -16,7 +16,7 @@ library(tidyverse)
 # Set working directory to Google Drive
 setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
 
-# Source in needed functions
+# Source in needed functions from Kara's plant_comp_functions.R
 source("/Users/moriahyoung/Documents/GitHub/warmXtrophic/kara/plant_response_scripts/plant_comp_functions.R")
 
 # Read in species list
@@ -66,20 +66,73 @@ change_colnames <- function(df){
         return(df)
 }
 
-order_colnames <- c("Site", "Year", "Date", "Plot", "Species", "Action")
-colnames_ordered <- function(df){
-        df <- df[,order_colnames]
+# change species names
+change_spp_phen <- function(df){
+        df$Species[df$Species == "Aspi "] <- "Aspi"
+        df$Species[df$Species == "Daca "] <- "Daca"
+        df$Species[df$Species == "Hype "] <- "Hype"
+        df$Species[df$Species == "Soca "] <- "Soca"
+        df$Species[df$Species == "Acmi "] <- "Acmi"
+        df$Species[df$Species == "ruac"] <- "Ruac"
+        df$Species[df$Species == "Ruace"] <- "Ruac"
+        df$Species[df$Species == "Cape "] <- "Cape"
+        df$Species[df$Species == "cape"] <- "Cape"
+        df$Species[df$Species == "Vaan "] <- "Vaan"
+        df$Species[df$Species == "Popr "] <- "Popr"
+        df$Species[df$Species == "Popre"] <- "Popr"
+        df$Species[df$Species == "Porp"] <- "Popr"
+        df$Species[df$Species == "PHpr"] <- "Phpr"
+        df$Species[df$Species == "Piau"] <- "Hiau"
+        df$Species[df$Species == "Des"] <- "Desp"
+        df$Species[df$Species == "rual"] <- "Rual"
+        df$Species[df$Species == "HIsp"] <- "Hisp"
+        df$Species[df$Species == "Hipr"] <- "Hica"
+        df$Species[df$Species == "Dach"] <- "Daca"
+        df$Species[df$Species == "Drin"] <- "Brin"
+        df$Species[df$Species == "Dafl"] <- "Dagl"
+        df$Species[df$Species == "unknown"] <- "Unknown"
         return(df)
-} # this won't work bc "undefined columns selected"
+}
+
+change_site_phen <- function(df){
+        df$Site[df$Site == "KBS"] <- "kbs"
+        df$Site[df$Site == "umbs "] <- "umbs"
+        df$Site[df$Site == "kbs "] <- "kbs"
+        return(df)
+}
+
+#order_colnames <- c("Site", "Year", "Date", "Plot", "Species", "Action")
+#colnames_ordered <- function(df){
+#        df <- df[,order_colnames]
+#        return(df)
+#} # this won't work bc "undefined columns selected"
 
 # Add dataframes into a list so that needed functions can be applied 
 phen_list <- list(kbs_2015=kbs_2015, kbs_2016=kbs_2016, kbs_2017=kbs_2017, kbs_2018=kbs_2018, kbs_2019=kbs_2019, kbs_2020=kbs_2020, 
                   umbs_2016=umbs_2016, umbs_2017=umbs_2017, umbs_2018=umbs_2018, umbs_2019=umbs_2019, umbs_2020=umbs_2020)
-phen_list <- lapply(phen_list, change_colnames) # this did not work for kbs_2015 & kbs_2016
+phen_list <- lapply(phen_list, change_colnames) 
 phen_list <- lapply(phen_list, remove_col, name=c("Julian", "Notes", "collector", "julian", "notes"))
-phen_list <- lapply(phen_list, colnames_ordered)
+#phen_list <- lapply(phen_list, colnames_ordered)
 phen_list <- lapply(phen_list, change_date)
-lapply(phen_list, spp_name)
+lapply(phen_list, spp_name) # look over species code to see what needs to be fixed
+lapply(phen_list, site_name) # need to make these all the same for each site
+
+# Fixing species names
+phen_list <- lapply(phen_list, change_spp_phen)
+lapply(phen_list, spp_name) # looks good
+
+
+spp_name <- function(df){
+        spp <- unique(sort(df[["Species"]]))
+        return(spp)
+}
+
+unique(dat$Species)
+setdiff(unique(dat$Species), unique(taxa$code))
+
+# Fixing site names
+phen_list <- lapply(phen_list, change_site_phen)
+lapply(phen_list, site_name) # looks good
 
 # Merge final data
 phen_merge <- rbind(phen_list$kbs_2015, phen_list$kbs_2016, phen_list$kbs_2017, phen_list$kbs_2018, phen_list$kbs_2019, phen_list$kbs_2020,
