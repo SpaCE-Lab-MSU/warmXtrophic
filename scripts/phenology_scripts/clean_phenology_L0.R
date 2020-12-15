c00f5a8b03c0fab44807e44e381e8a565746ea8f
Moriah Young <moriahyoung@Moriahs-MacBook-Air.local>
2020-12-15 13:17:09 -0500

    created a functions script and edited cleaning script

--- scripts/phenology_scripts/clean_phenology_L0.R
+++ scripts/phenology_scripts/clean_phenology_L0.R
@@ -17,10 +17,10 @@ library(tidyverse)
 setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
 
 # Source in needed functions from Kara's plant_comp_functions.R
-source("/Users/moriahyoung/Documents/GitHub/warmXtrophic/kara/plant_response_scripts/plant_comp_functions.R")
+source("/Users/moriahyoung/Documents/GitHub/warmXtrophic/scripts/phenology_scripts/phenology_functions.R")
 
 # Read in species list
-taxa <- read.csv("L2/taxon_uptodate.csv", stringsAsFactors=F)
+#taxa <- read.csv("L2/taxon_uptodate.csv", stringsAsFactors=F)
 
 # Read in data
 kbs_2015 <- read.csv("L0/KBS/2015/kbs_flwr_sd_2015.csv")
@@ -39,67 +39,22 @@ umbs_2020 <- read.csv("L0/UMBS/2020/umbs_flwr_sd_2020.csv")
 kbs_2016$Site <- "kbs"
 umbs_2016$Site <- "umbs"
 
-# Add a "Year" column for kbs and umbs for all years
-kbs_2015$Year <- 2015
-kbs_2016$Year <- 2016
-kbs_2017$Year <- 2017
-kbs_2018$Year <- 2018
-kbs_2019$Year <- 2019
-kbs_2020$Year <- 2020
-umbs_2016$Year <- 2016
-umbs_2017$Year <- 2017
-umbs_2018$Year <- 2018
-umbs_2019$Year <- 2019
-umbs_2020$Year <- 2020
-
+# Add a "Year" column for kbs and umbs for all years 
+# Don't need to do this here... but you can
+#kbs_2015$Year <- 2015
+#kbs_2016$Year <- 2016
+#kbs_2017$Year <- 2017
+#kbs_2018$Year <- 2018
+#kbs_2019$Year <- 2019
+#kbs_2020$Year <- 2020
+#umbs_2016$Year <- 2016
+#umbs_2017$Year <- 2017
+#umbs_2018$Year <- 2018
+#umbs_2019$Year <- 2019
+#umbs_2020$Year <- 2020
 
 # Change column name for kbs 2015
 colnames(kbs_2015) <- sub("event", "Action", colnames(kbs_2015))
-
-#function to change column names
-change_colnames <- function(df){
-        names(df)[names(df) == "site"] <- "Site"
-        names(df)[names(df) == "date"] <- "Date"
-        names(df)[names(df) == "plot"] <- "Plot"
-        names(df)[names(df) == "species"] <- "Species"
-        names(df)[names(df) == "action"] <- "Action"
-        return(df)
-}
-
-# change species names
-change_spp_phen <- function(df){
-        df$Species[df$Species == "Aspi "] <- "Aspi"
-        df$Species[df$Species == "Daca "] <- "Daca"
-        df$Species[df$Species == "Hype "] <- "Hype"
-        df$Species[df$Species == "Soca "] <- "Soca"
-        df$Species[df$Species == "Acmi "] <- "Acmi"
-        df$Species[df$Species == "ruac"] <- "Ruac"
-        df$Species[df$Species == "Ruace"] <- "Ruac"
-        df$Species[df$Species == "Cape "] <- "Cape"
-        df$Species[df$Species == "cape"] <- "Cape"
-        df$Species[df$Species == "Vaan "] <- "Vaan"
-        df$Species[df$Species == "Popr "] <- "Popr"
-        df$Species[df$Species == "Popre"] <- "Popr"
-        df$Species[df$Species == "Porp"] <- "Popr"
-        df$Species[df$Species == "PHpr"] <- "Phpr"
-        df$Species[df$Species == "Piau"] <- "Hiau"
-        df$Species[df$Species == "Des"] <- "Desp"
-        df$Species[df$Species == "rual"] <- "Rual"
-        df$Species[df$Species == "HIsp"] <- "Hisp"
-        df$Species[df$Species == "Hipr"] <- "Hica"
-        df$Species[df$Species == "Dach"] <- "Daca"
-        df$Species[df$Species == "Drin"] <- "Brin"
-        df$Species[df$Species == "Dafl"] <- "Dagl"
-        df$Species[df$Species == "unknown"] <- "Unknown"
-        return(df)
-}
-
-change_site_phen <- function(df){
-        df$Site[df$Site == "KBS"] <- "kbs"
-        df$Site[df$Site == "umbs "] <- "umbs"
-        df$Site[df$Site == "kbs "] <- "kbs"
-        return(df)
-}
 
 #order_colnames <- c("Site", "Year", "Date", "Plot", "Species", "Action")
 #colnames_ordered <- function(df){
@@ -118,20 +73,20 @@ lapply(phen_list, spp_name) # look over species code to see what needs to be fix
 lapply(phen_list, site_name) # need to make these all the same for each site
 
 # Fixing species names
-phen_list <- lapply(phen_list, change_spp_phen)
+phen_list <- lapply(phen_list, change_spp)
 lapply(phen_list, spp_name) # looks good
 
-
-spp_name <- function(df){
-        spp <- unique(sort(df[["Species"]]))
-        return(spp)
-}
-
-unique(dat$Species)
-setdiff(unique(dat$Species), unique(taxa$code))
+# Would be great to write some code where you can compare the species codes in the data with
+# our species list
+#spp_name <- function(df){
+#        spp <- unique(sort(df[["Species"]]))
+#        return(spp)
+#}
+#unique(dat$Species)
+#setdiff(unique(dat$Species), unique(taxa$code))
 
 # Fixing site names
-phen_list <- lapply(phen_list, change_site_phen)
+phen_list <- lapply(phen_list, change_site)
 lapply(phen_list, site_name) # looks good
 
 # Merge final data
---
+++ scripts/phenology_scripts/phenology_functions.R
@@ -0,0 +1,78 @@
+# TITLE:          Phenology functions
+# AUTHORS:        Kara Dobson and Moriah Young 
+# COLLABORATORS:  Phoebe Zarnetske, Mark Hammond
+# DATA INPUT:     
+# DATA OUTPUT:    Script containing functions for cleaning phenology data
+# PROJECT:        warmXtrophic
+# DATE:           December, 2020
+
+# remove certain columns
+remove_col <- function(df,name){
+        vec <- which(names(df) %in% name)
+        df = df[,-vec]
+        return(df)
+}
+
+# change date format
+change_date <- function(df){
+        df[["Date"]] <- as.Date(df[["Date"]],format="%m/%d/%Y")
+        return(df)
+}
+
+# check that there are no species misspellings
+spp_name <- function(df){
+        spp <- unique(sort(df[["Species"]]))
+        return(spp)
+}
+
+# check that there are no site name misspellings
+site_name <- function(df){
+        spp <- unique(sort(df[["Site"]]))
+        return(spp)
+}
+
+#function to change column names
+change_colnames <- function(df){
+        names(df)[names(df) == "site"] <- "Site"
+        names(df)[names(df) == "date"] <- "Date"
+        names(df)[names(df) == "plot"] <- "Plot"
+        names(df)[names(df) == "species"] <- "Species"
+        names(df)[names(df) == "action"] <- "Action"
+        return(df)
+}
+
+# change species names
+change_spp <- function(df){
+        df$Species[df$Species == "Aspi "] <- "Aspi"
+        df$Species[df$Species == "Daca "] <- "Daca"
+        df$Species[df$Species == "Hype "] <- "Hype"
+        df$Species[df$Species == "Soca "] <- "Soca"
+        df$Species[df$Species == "Acmi "] <- "Acmi"
+        df$Species[df$Species == "ruac"] <- "Ruac"
+        df$Species[df$Species == "Ruace"] <- "Ruac"
+        df$Species[df$Species == "Cape "] <- "Cape"
+        df$Species[df$Species == "cape"] <- "Cape"
+        df$Species[df$Species == "Vaan "] <- "Vaan"
+        df$Species[df$Species == "Popr "] <- "Popr"
+        df$Species[df$Species == "Popre"] <- "Popr"
+        df$Species[df$Species == "Porp"] <- "Popr"
+        df$Species[df$Species == "PHpr"] <- "Phpr"
+        df$Species[df$Species == "Piau"] <- "Hiau"
+        df$Species[df$Species == "Des"] <- "Desp"
+        df$Species[df$Species == "rual"] <- "Rual"
+        df$Species[df$Species == "HIsp"] <- "Hisp"
+        df$Species[df$Species == "Hipr"] <- "Hica"
+        df$Species[df$Species == "Dach"] <- "Daca"
+        df$Species[df$Species == "Drin"] <- "Brin"
+        df$Species[df$Species == "Dafl"] <- "Dagl"
+        df$Species[df$Species == "unknown"] <- "Unknown"
+        return(df)
+}
+
+change_site <- function(df){
+        df$Site[df$Site == "KBS"] <- "kbs"
+        df$Site[df$Site == "umbs "] <- "umbs"
+        df$Site[df$Site == "kbs "] <- "kbs"
+        return(df)
+}
+
