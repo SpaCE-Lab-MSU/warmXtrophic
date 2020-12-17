ba6c2791d6afee55076e5cc43549dd38ea345ae9
Kara Dobson <dobsonk2@msu.edu>
2020-12-17 12:24:25 -0500

    made a cleanup script for herbivory

---
+++ scripts/herbivory_scripts/herbivory_clean_L0.R
@@ -0,0 +1,7 @@
+# TITLE:          Herbivory cleanup
+# AUTHORS:        Kara Dobson
+# COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Moriah Young
+# DATA INPUT:     Data imported as csv files from shared Google drive L0 folder
+# DATA OUTPUT:    
+# PROJECT:        warmXtrophic
+# DATE:           December, 2020
