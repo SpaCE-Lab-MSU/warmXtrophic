411e43ec84e272696831552a929e7810b65196c6
moriahy <yomoriah@gmail.com>
2021-01-19 15:59:54 -0500

    deleting cleaning code

--- scripts/phenology_scripts/phenology_plots.R
+++ scripts/phenology_scripts/phenology_plots.R
@@ -18,55 +18,24 @@ setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
 
 ############################### Read in data 
 # cleaned phenology data from L1
-phen_data <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L1/reproductive_phenology/final_flw_sd_L1.csv")
+phen_data <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L1/phenology/final_flw_sd_L1.csv", stringsAsFactors=FALSE)
 phen_data <- phen_data %>% 
-        select(-X)
-
-# Change column names to lowercase
-names(phen_data) <- tolower(names(phen_data))
-str(phen_data)
-
-# Fix date column & add column for the year and julian day
-phen_data$date <- as.Date(phen_data$date, format="%Y-%m-%d")
-str(phen_data)
-phen_data$year <- format(phen_data$date,format="%Y")
-phen_data$julian <- format(phen_data$date, "%j")
-
-# plot level information from L2
-plot_meta <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L2/plot.csv")
-
-# taxon data from L2
-taxa <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L2/taxon.csv")
-# Change column name for from "code" to "Species" to match cleaned phenology data
-colnames(taxa) <- sub("code", "species", colnames(taxa))
-
-############################# Merge data and clean 
-# merge cleaned phen_data with the plot level information
-phen_data2 <- merge(phen_data, plot_meta, by = "plot")
-phen_data3 <- merge(phen_data2, taxa, by = "species")
-
-phen_data <- phen_data3 %>% 
-        select(-old_name, -old_species, -site.y, -resolution)
-colnames(phen_data) <- sub("site.x", "site", colnames(phen_data))
-
-# Change column names to lowercase (again)
-names(phen_data) <- tolower(names(phen_data))
-str(phen_data)
-
-# Make julian column numeric
-phen_data$julian <- as.numeric(phen_data$julian)
-str(phen_data)
+        select(-X) # get rid of "X" column that shows up
 
 # Create separate data frames for flowering and seeding
-phen_data_flwr <- subset(phen_data, action == "flower")
-phen_data_sd <- subset(phen_data, action == "seed")
+phen_flwr <- subset(phen_data, action == "flower")
+phen_sd <- subset(phen_data, action == "seed")
 
 # This creates a data frame that returns the first date of flower per species per plot
 # Filter data to contain the date of first occurrence for each species
 
-FirstFlower <- phen_data_flwr %>%
+FirstFlower <- phen_flwr %>%
         group_by(plot, year, species, state, site, action) %>%
         summarize(julian = min(julian, na.rm=T))
+
+FirstFlwr <- phen_flwr %>% 
+        group_by(year, state, site, action) %>% 
+        summerize(julian = min(julian, na.rm=T))
 
 sum_FirstFlower <- FirstFlower %>%
         group_by(site, state, species, year) %>%
@@ -87,9 +56,11 @@ FirstFlower_plot <- function(spp, loc) {
 }
 
 FirstFlower_plot("Popr", "umbs")
-FirstFlower_plot("Cest", "umbs")
+FirstFlower_plot("Popr", "kbs")
 FirstFlower_plot("Eugr", "kbs")
 FirstFlower_plot("Soca", "kbs")
+
+# Calculate the average date of first flower for all ambient and warmed plots per year per site. Plot.
 
 # create a function that returns plots (1 for each year at each site) for a given site and year comparing
 # warmed vs ambient plots for average first date of flower (add by functional groups next?)
