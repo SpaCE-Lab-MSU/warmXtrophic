459f7089b4107e63af6eb22d2a3b1143d7b7af95
moriahy <yomoriah@gmail.com>
2021-01-19 18:56:12 -0500

    worte another plot script

--- scripts/phenology_scripts/phenology_plots.R
+++ scripts/phenology_scripts/phenology_plots.R
@@ -12,6 +12,7 @@ rm(list=ls())
 #Load packages
 library(tidyverse)
 library(plotrix)
+library(cowplot)
 
 # Set working directory to Google Drive
 setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
@@ -32,15 +33,17 @@ phen_sd <- subset(phen_data, action == "seed")
 FirstFlower <- phen_flwr %>%
         group_by(plot, year, species, state, site, action) %>%
         summarize(julian = min(julian, na.rm=T))
-
-FirstFlwr <- phen_flwr %>% 
-        group_by(year, state, site, action) %>% 
-        summerize(julian = min(julian, na.rm=T))
-
+        
 sum_FirstFlower <- FirstFlower %>%
         group_by(site, state, species, year) %>%
         summarize(avg_julian = mean(julian, na.rm = TRUE),
                   se = std.error(julian, na.rm = TRUE))
+
+sum_FirstFlwr <- FirstFlower %>%
+        group_by(site, state, year) %>%
+        summarize(avg_julian = mean(julian, na.rm = TRUE),
+                  se = std.error(julian, na.rm = TRUE)) 
+# this gives the average julian day of first flower for each site and year for warmed and ambient
 
 FirstFlower_plot <- function(spp, loc) { 
         FirstFlower_spp <- subset(sum_FirstFlower, species == spp & site == loc)
@@ -60,7 +63,39 @@ FirstFlower_plot("Popr", "kbs")
 FirstFlower_plot("Eugr", "kbs")
 FirstFlower_plot("Soca", "kbs")
 
-# Calculate the average date of first flower for all ambient and warmed plots per year per site. Plot.
+sum_FirstFlwr_plot <- function(loc, yr) { 
+        FirstFlwr_sub <- subset(sum_FirstFlwr, site == loc & year == yr)
+        return(ggplot(FirstFlwr_sub, aes(x = state, y = avg_julian, fill = state)) +
+                       facet_grid(.~year) +
+                       geom_bar(position = "identity", stat = "identity") +
+                       geom_errorbar(aes(ymin = avg_julian - se, ymax = avg_julian + se), width = 0.2,
+                                     position = "identity") +
+                       labs(x = "State", y = "Julian Day of First Flower", title = year, subtitle = loc) +
+                       scale_fill_manual(values = c("blue", "darkred")) +
+                       scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+                       theme_grey())
+} # this creates a function that returns plots for a given site and year for average first date of flower comparing ambient vs warmed plots
+
+# kbs plots
+kbs_2015 <- sum_FirstFlwr_plot("kbs", "2015")
+kbs_2016 <- sum_FirstFlwr_plot("kbs", "2016")
+kbs_2017 <- sum_FirstFlwr_plot("kbs", "2017")
+kbs_2018 <- sum_FirstFlwr_plot("kbs", "2018")
+kbs_2019 <- sum_FirstFlwr_plot("kbs", "2019")
+kbs_2020 <- sum_FirstFlwr_plot("kbs", "2020")
+
+plot_grid(kbs_2015, kbs_2016, kbs_2017, kbs_2018, kbs_2019, kbs_2020,
+          ncol = 3, nrow = 2)
+
+# umbs plots
+umbs_2016 <- sum_FirstFlwr_plot("umbs", "2016")
+umbs_2017 <- sum_FirstFlwr_plot("umbs", "2017")
+umbs_2018 <- sum_FirstFlwr_plot("umbs", "2018")
+umbs_2019 <- sum_FirstFlwr_plot("umbs", "2019")
+umbs_2020 <- sum_FirstFlwr_plot("umbs", "2020")
+
+plot_grid(umbs_2016, umbs_2017, umbs_2018, umbs_2019, umbs_2020,
+          ncol = 3, nrow = 2)
 
 # create a function that returns plots (1 for each year at each site) for a given site and year comparing
 # warmed vs ambient plots for average first date of flower (add by functional groups next?)
