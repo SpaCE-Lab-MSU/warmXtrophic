3fd24ee16df9ec012f43d440a3a74bf65ee77a3c
moriahy <yomoriah@gmail.com>
2021-01-20 09:55:15 -0500

    created phenology analysis file and updated plot Rmd

---
+++ scripts/phenology_scripts/phenology_analysis.R
@@ -0,0 +1,13 @@
+# TITLE:          Phenology Data Analysis
+# AUTHORS:        Moriah Young
+# COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Kara Dobson
+# DATA INPUT:     Data imported as csv files from shared Google drive L1 folder
+# DATA OUTPUT:    
+# PROJECT:        warmXtrophic
+# DATE:           January, 2020
+
+# Clear all existing data
+rm(list=ls())
+
+#Load packages
+library(tidyverse)
