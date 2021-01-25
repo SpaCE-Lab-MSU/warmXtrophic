301a06b9988b7089f50310f80f8c163143a5b100
moriahy <yomoriah@gmail.com>
2021-01-25 10:42:57 -0500

    changed file name and added umbs files to clean

--- scripts/ANPP_scripts/biomass_clean_L0.R
+++ scripts/ANPP_scripts/biomass_clean_L0.R
@@ -23,6 +23,8 @@ kbs_biomass <- read.csv("L0/KBS/2020/kbs_ancillary_biomass_2020.csv")
 kbs_plant_comp <- read.csv("L0/KBS/2020/kbs_ancillary_plantcomp_2020.csv")
 
 # UMBS
+umbs_biomass <- read.csv("L0/UMBS/2020/umbs_ancillary_ANPP_2020.csv")
+umbs_plant_comp <- read.csv("L0/UMBS/2020/umbs_ancillary_plantcomp_2020.csv")
 
 # Clean data
 ## KBS
@@ -39,18 +41,49 @@ str(kbs_plant_comp)
 names(kbs_plant_comp) <- tolower(names(kbs_plant_comp))
 str(kbs_plant_comp)
 
-kbs_ANPP <- merge(kbs_biomass, kbs_plant_comp, by = c("plot", "species"))
+#kbs_ANPP <- merge(kbs_biomass, kbs_plant_comp, by = c("plot", "species"))
+kbs_ANPP <- full_join(kbs_biomass, kbs_plant_comp, by = c("plot", "species"))
+
 kbs_ANPP <- kbs_ANPP %>% 
-        select(-site.y, -date.x, )
+        select(-site.y, -date.x)
 colnames(kbs_ANPP) <- sub("site.x", "site", colnames(kbs_ANPP))
 colnames(kbs_ANPP) <- sub("date.y", "date", colnames(kbs_ANPP))
 
 colnames(kbs_ANPP) <- sub("final_biomass_g", "biomass", colnames(kbs_ANPP))
-View(kbs_ANPP)
 
 kbs_ANPP <- kbs_ANPP[, c("site", "date", "plot", "species", "cover", "biomass")]
+View(kbs_ANPP)
 
 ## UMBS
+View(umbs_biomass)
+umbs_biomass <- umbs_biomass %>% 
+        select(-dry_weight..g., -X, -dry_weight_kbs, -X.1, -dried.bag.weight, -bag, -notes, -X.2)
+
+umbs_biomass <- umbs_biomass[-c(93, 94, 95),]
+
+umbs_plant_comp <- umbs_plant_comp %>% 
+        select(-Julian, -Notes)
+str(umbs_plant_comp)
+
+# Change column names to lowercase so that we can merge with the biomass file
+names(umbs_plant_comp) <- tolower(names(umbs_plant_comp))
+str(umbs_plant_comp)
+
+#umbs_ANPP <- merge(umbs_biomass, umbs_plant_comp, by = c("plot", "species"))
+umbs_ANPP <- full_join(umbs_biomass, umbs_plant_comp, by = c("plot", "species"))
+
+umbs_ANPP <- umbs_ANPP %>% 
+        select(-site.y, -date.x)
+colnames(umbs_ANPP) <- sub("site.x", "site", colnames(umbs_ANPP))
+colnames(umbs_ANPP) <- sub("date.y", "date", colnames(umbs_ANPP))
+
+colnames(umbs_ANPP) <- sub("weight_g", "biomass", colnames(umbs_ANPP))
+
+umbs_ANPP <- umbs_ANPP[, c("site", "date", "plot", "species", "cover", "biomass")]
+View(umbs_ANPP)
+
+# Merge the kbs and umbs files together
+final_ANPP <- merge(kbs_ANPP, umbs_ANPP, by = c("site", "plot", "species"))
 
 # write a new cvs with the cleaned and merge data and upload to the shared google drive in L1
-write.csv(kbs_ANPP, file="L1/final_ANPP.csv")                     
+write.csv(final_ANPP, file="L1/final_ANPP.csv")                     
