9f8564a392b34c6d7cc05037a4718a7635d670b8
Kara Dobson <dobsonk2@msu.edu>
2021-01-25 10:30:35 -0500

    updated some plots & analyses

--- scripts/herbivory_scripts/herbivory_plots.R
+++ scripts/herbivory_scripts/herbivory_plots.R
@@ -30,23 +30,23 @@ herb <- subset(herb, select = -X)
 
 
 
-#### Total herb by site and species (ignoring insecticide) ####
+#### Total herb by site and species ####
 sum_herb <- herb %>%
-  group_by(site, state, species, year) %>%
+  group_by(site, treatment_key, species, year) %>%
   summarize(avg_eaten = mean(p_eaten, na.rm = TRUE),
             se = std.error(p_eaten, na.rm = TRUE))
 
 # Function to make a plot for any species
 herb_plot <- function(spp, loc) { 
   herb_spp <- subset(sum_herb, species == spp & site == loc)
-  return(ggplot(herb_spp, aes(x = state, y = avg_eaten, fill = state)) +
+  return(ggplot(herb_spp, aes(x = treatment_key, y = avg_eaten, fill = treatment_key)) +
            facet_grid(.~year) +
            geom_bar(position = "identity", stat = "identity") +
            geom_errorbar(aes(ymin = avg_eaten - se, ymax = avg_eaten + se), width = 0.2,
                          position = "identity") +
            labs(x = "State", y = "Average Percent of Leaf Eaten", title = spp) +
-           scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
-           scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+           #scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+           #scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
            theme_classic())
 }
 herb_plot("Popr", "umbs")
@@ -54,14 +54,13 @@ herb_plot("Eugr", "kbs")
 herb_plot("Soca", "kbs")
 
 
-############### FIX #################
+
 #### Total herb by site and species with insecticide treatment####
 sum_herb_in <- herb %>%
   group_by(site, treatment_key, year) %>%
   summarize(avg_eaten = mean(p_eaten, na.rm = TRUE),
             se = std.error(p_eaten, na.rm = TRUE))
 
-# Function to make a plot for any species
 herb_plot_in <- function(loc) { 
   herb_spp <- subset(sum_herb_in, site == loc)
   return(ggplot(herb_spp, aes(x = treatment_key, y = avg_eaten, fill = treatment_key)) +
@@ -77,25 +76,46 @@ herb_plot_in("umbs")
 herb_plot_in("kbs")
 
 
-
-#### Total herb by site ####
-sum_herb_all <- herb %>%
-  group_by(site, state, year) %>%
+#### Total herb by site and species with separated insecticide and no insecticide####
+sum_herb_in <- herb %>%
+  group_by(site, treatment_key, year) %>%
   summarize(avg_eaten = mean(p_eaten, na.rm = TRUE),
             se = std.error(p_eaten, na.rm = TRUE))
 
-# Plot for all species between warmed and ambient
-herb_plot_all <- function(loc) { 
-  herb_spp <- subset(sum_herb_all, site == loc)
-  return(ggplot(herb_spp, aes(x = state, y = avg_eaten, fill = state)) +
+herb_plot_in <- function(loc, key, key2) { 
+  herb_spp <- subset(sum_herb_in, site == loc & treatment_key == key | site== loc & treatment_key == key2)
+  return(ggplot(herb_spp, aes(x = treatment_key, y = avg_eaten, fill = treatment_key)) +
            facet_grid(.~year) +
            geom_bar(position = "identity", stat = "identity") +
            geom_errorbar(aes(ymin = avg_eaten - se, ymax = avg_eaten + se), width = 0.2,
                          position = "identity") +
-           labs(x = "State", y = "Average Percent of Leaf Eaten", title = loc) +
-           scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
-           scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+           labs(x = "Treatment", y = "Average Percent of Leaf Eaten", title = loc) +
+           #scale_fill_manual(values = c("#abd9e9", "#2c7bb6", "#fdae61", "#d7191c")) +
            theme_classic())
 }
-herb_plot_all("umbs")
-herb_plot_all("kbs")
+herb_plot_in("umbs", "A0", "W0")
+herb_plot_in("kbs", "A0", "W0")
+herb_plot_in("umbs", "AI", "WI")
+herb_plot_in("kbs", "AI", "WI")
+
+#### Total herb by site ####
+#sum_herb_all <- herb %>%
+#  group_by(site, state, year) %>%
+#  summarize(avg_eaten = mean(p_eaten, na.rm = TRUE),
+#            se = std.error(p_eaten, na.rm = TRUE))
+#
+# Plot for all species between warmed and ambient - doesn't account for insecticide treatment
+#herb_plot_all <- function(loc) { 
+#  herb_spp <- subset(sum_herb_all, site == loc)
+#  return(ggplot(herb_spp, aes(x = state, y = avg_eaten, fill = state)) +
+#           facet_grid(.~year) +
+#           geom_bar(position = "identity", stat = "identity") +
+#           geom_errorbar(aes(ymin = avg_eaten - se, ymax = avg_eaten + se), width = 0.2,
+#                         position = "identity") +
+#           labs(x = "State", y = "Average Percent of Leaf Eaten", title = loc) +
+#           scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+#           scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+#           theme_classic())
+#}
+#herb_plot_all("umbs")
+#herb_plot_all("kbs")
--- scripts/plant_comp_scripts/plant_comp_analyses.R
+++ scripts/plant_comp_scripts/plant_comp_analyses.R
@@ -13,33 +13,106 @@ rm(list=ls())
 library(tidyverse)
 library(lme4)
 library(nlme)
+library(lmerTest)
 
 # Set working directory to Google Drive
 # **** Update with the path to your Google drive on your computer
 setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
 
-# Read in plant comp data & metadata
+# Read in plant comp data
 plant_comp <- read.csv("L1/plant_composition/final_plantcomp_L1.csv")
+meta <- read.csv("L0/plot.csv")
+str(plant_comp) # for some reason, date column converted back to character
 
-# First date by site
-filter_comp <- plant_comp %>%
-  group_by(plot, year, species, state, insecticide, treatment_key, site) %>%
-  summarize(julian = min(julian))
+# Fix date column
+plant_comp$date <- as.Date(plant_comp$date,format="%Y-%m-%d")
+str(plant_comp)
+
+# split the plant_comp dataframe
+dataus <- split(x = plant_comp, f = plant_comp[, c("plot", "species","site", "year")])
+
+# Determine dates for each plot-species combination where the value of `cover` is at least half the max value
+half_cover_dates <- unlist(lapply(X = dataus, FUN = function(x){
+  x[which.max(x[["cover"]] >= max(x[["cover"]])/2), "julian"]
+}))
+
+# make into a dataframe
+half_cover_dates_df <- data.frame("plot.species.site.year" = names(half_cover_dates),
+                                  "half_cover_date" = unname(half_cover_dates), stringsAsFactors = FALSE)
+
+# fix species and plot column
+half_cover_dates_df[["plot"]] <- sapply(X = strsplit(x = half_cover_dates_df[["plot.species.site.year"]], split = ".", fixed = TRUE), FUN = `[`, 1L)
+half_cover_dates_df[["species"]] <- sapply(X = strsplit(x =half_cover_dates_df[["plot.species.site.year"]], split = ".", fixed = TRUE), FUN = `[`, 2L)
+half_cover_dates_df[["site"]] <- sapply(X = strsplit(x =half_cover_dates_df[["plot.species.site.year"]], split = ".", fixed = TRUE), FUN = `[`, 3L)
+half_cover_dates_df[["year"]] <- sapply(X = strsplit(x =half_cover_dates_df[["plot.species.site.year"]], split = ".", fixed = TRUE), FUN = `[`, 4L)
+half_cover_dates_df$plot.species.site.year <- NULL
+
+# determine first date of emergence for correlation with 'green-up' index
+min_date <- aggregate(plant_comp$julian,by=plant_comp[,c("plot","species")],FUN=min)
+colnames(min_date) <- c("plot", "species", "min_emerg_date")
+
+# merge min date dateframe with "green-up index" df
+combined <- merge(half_cover_dates_df, min_date, by=c("plot", "species"))
+
+# calculate correlation
+cor.test(combined$min_emerg_date, combined$half_cover_date)
+
+# re-merge data with meta data info
+final <- left_join(meta, combined, by = "plot")
+
+# remove uneeded columns
+final$date <- NULL
+final$species.y <- NULL
+final$cover <- NULL
+final$julian <- NULL
+final$X <- NULL
+final$site.y <- NULL
+final$year.y <- NULL
+
+# fix column names
+colnames(final)[which(names(final) == "species.x")] <- "species"
+colnames(final)[which(names(final) == "site.x")] <- "site"
+colnames(final)[which(names(final) == "year.x")] <- "year"
+
+# create dataframes for kbs and umbs
+final_kbs <- subset(final, site == "kbs")
+final_umbs <- subset(final, site == "umbs")
+
+
+
+## partially taken from kileighs old models ##
+# do we need plot as a random effect?
+moda <- lmer(half_cover_date ~ state + insecticide + (1|species) + (1|plot), final_kbs)
+modb <- lmer(half_cover_date ~ treatment_key + (1|species), final_kbs)
+anova(modb, moda) #yes
+summary(modb)
+anova(modb)
+
+# do we need insects?
+modc <- lmer(half_cover_date ~ state + (1|species) + (1|plot), final_kbs, REML=FALSE)
+anova(moda, modc) #yes
 
 
 ##### attempting different models #######
 ### comparing linear vs mixed effects ###
 # linear model
-lm1 <- lm(julian~state*insecticide, data = filter_comp)
+lm1 <- lm(half_cover_date~state*insecticide, data = final_kbs)
 summary(lm1)
+
+lm2 <- lm(half_cover_date~state*insecticide, data = final_umbs)
+summary(lm2)
 
 # mixed effects model -> after running lme with plot as random effect, may not be needed (plot doesn't affect intercepts in coef)
 # below does not give p values from lme4, so I switched to nlme instead
 #lme1 <- lmer(julian~state*insecticide + (1 | plot), data=filter_comp)
 #summary(lme1)
-lme1 <- lme(julian~state*insecticide, random=~1|plot, data = filter_comp)
-summary(lme1)
-coef(lme1)
+lme2 <- lme(half_cover_date~state*insecticide, random=~1|year, data = final_kbs)
+summary(lme2)
+coef(lme2)
+
+lme3 <- lme(half_cover_date~state*insecticide, random=~1|plot, data = final_umbs)
+summary(lme3)
+coef(lme3)
 
 # note to self: include species as random effect?
 # no sig difference between warmed and ambient with or without insecticide
