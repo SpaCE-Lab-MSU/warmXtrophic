f37c154b23bf1d196f35bc6b69d19c776efea0a8
Kara Dobson <dobsonk2@msu.edu>
2021-03-24 13:27:42 -0400

    updated plant comp species

--- scripts/plant_comp_scripts/plant_comp_clean_L0.R
+++ scripts/plant_comp_scripts/plant_comp_clean_L0.R
@@ -48,6 +48,52 @@ umbs_2016$Site <- "umbs"
 # Change column name for umbs 2016
 colnames(umbs_2016) <- sub("Percent_Cover", "Cover", colnames(umbs_2016))
 
+# Could make a function for these below, TBD
+# remove a species from KBS (and not UMBS)
+kbs_2015 <- kbs_2015[!grepl("Desp",kbs_2015$Species),]
+kbs_2016 <- kbs_2016[!grepl("Desp",kbs_2016$Species),]
+kbs_2017 <- kbs_2017[!grepl("Desp",kbs_2017$Species),]
+kbs_2018 <- kbs_2018[!grepl("Desp",kbs_2018$Species),]
+kbs_2019 <- kbs_2019[!grepl("Desp",kbs_2019$Species),]
+kbs_2020 <- kbs_2020[!grepl("Desp",kbs_2020$Species),]
+kbs_2020 <- kbs_2020[!grepl("Des",kbs_2020$Species),]
+
+# remove species from UMBS (and not KBS)
+umbs_2015 <- umbs_2015[!grepl("Vear",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Vear",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Vear",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Vear",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Vear",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Vear",umbs_2020$Species),]
+
+umbs_2015 <- umbs_2015[!grepl("Elre",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Elre",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Elre",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Elre",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Elre",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Elre",umbs_2020$Species),]
+
+umbs_2015 <- umbs_2015[!grepl("Rusp",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Rusp",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Rusp",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Rusp",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Rusp",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Rusp",umbs_2020$Species),]
+
+umbs_2015 <- umbs_2015[!grepl("Ruag",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Ruag",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Ruag",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Ruag",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Ruag",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Ruag",umbs_2020$Species),]
+
+umbs_2015 <- umbs_2015[!grepl("Rual",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Rual",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Rual",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Rual",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Rual",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Rual",umbs_2020$Species),]
+
 # Add dataframes into a list so that needed functions can be applied 
 comp_list <- list(kbs_2015=kbs_2015, kbs_2016=kbs_2016, kbs_2017=kbs_2017, kbs_2018=kbs_2018, kbs_2019=kbs_2019, kbs_2020=kbs_2020, 
                   umbs_2015=umbs_2015, umbs_2016=umbs_2016, umbs_2017=umbs_2017, umbs_2018=umbs_2018, umbs_2019=umbs_2019, umbs_2020=umbs_2020)
@@ -57,18 +103,22 @@ lapply(comp_list, spp_name) # need to fix a few species names
 lapply(comp_list, site_name) # need to make these all the same for each site
 
 # Fixing species names
-# Ruag (Rual), Smooth_oat (Arre? Arel?), Cofo (?)
 comp_list <- lapply(comp_list, change_spp)
-lapply(comp_list, spp_name) # looks good
+lapply(comp_list, spp_name)
+
+# Remove species
+comp_list <- lapply(comp_list, remove_spp)
+lapply(comp_list, spp_name)
 
 # Fixing site name
 comp_list <- lapply(comp_list, change_site)
-lapply(comp_list, site_name) # looks good
+lapply(comp_list, site_name)
 
 # Merge final data
 comp_merge <- rbind(comp_list$kbs_2015, comp_list$kbs_2016, comp_list$kbs_2017, comp_list$kbs_2018, comp_list$kbs_2019, comp_list$kbs_2020, 
                     comp_list$umbs_2015, comp_list$umbs_2016, comp_list$umbs_2017, comp_list$umbs_2018, comp_list$umbs_2019, comp_list$umbs_2020)
 str(comp_merge)
+sort(unique(comp_merge$Species))
 
 # Change column names to lowercase so they can be merged with metadata
 names(comp_merge) <- tolower(names(comp_merge))
@@ -90,19 +140,24 @@ colnames(taxon)[which(names(taxon) == "code")] <- "species"
 # Merge meta-data with plant comp data 
 # Make sure the plot IDs are the same in each, and there aren't any missing
 sort(unique(comp_merge$plot))
-sort(unique(comp_merge$plot))
 plant_comp_merge <- left_join(meta, comp_merge, by = "plot")
+sort(unique(plant_comp_merge$species))
 
 # taxon contains "site" which is the site where the species is found on our meta-data table, but those data exist in our plant_comp_merge dataset already.
 # Delete "site" from taxon so it doesn't accidentally get merged in.
 taxon$site<-NULL
 plant_comp_merge2 <- left_join(taxon, plant_comp_merge, by = "species")
+sort(unique(plant_comp_merge2$species))
+
+# remove species not found in our dataset
+plant_comp_merge3 <- plant_comp_merge2[!is.na(plant_comp_merge2$cover),]
+sort(unique(plant_comp_merge3$species))
 
 # remove unnecessary columns
-plant_comp_merge2$old_code <- NULL
-plant_comp_merge2$old_name <- NULL
-plant_comp_merge2$resolution <- NULL
+plant_comp_merge3$old_code <- NULL
+plant_comp_merge3$old_name <- NULL
+plant_comp_merge3$resolution <- NULL
 
 # Upload clean data csv to google drive without the index column
-write.csv(plant_comp_merge2, file="L1/plant_composition/final_plantcomp_L1.csv", row.names=FALSE)
+write.csv(plant_comp_merge3, file="L1/plant_composition/final_plantcomp_L1.csv", row.names=FALSE)
 
