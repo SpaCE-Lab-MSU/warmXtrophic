fc768468dd6c437c4395fde65fbb3c29aa8adf2b
Kara Dobson <dobsonk2@msu.edu>
2021-03-24 12:30:44 -0400

    fixing plant comp species

--- scripts/plant_comp_scripts/plant_comp_functions.R
+++ scripts/plant_comp_scripts/plant_comp_functions.R
@@ -31,16 +31,54 @@ site_name <- function(df){
   return(spp)
 }
 
+# remove species
+remove_spp <- function(df){
+        df <- df[!grepl("Alpe",df$Species),]
+        df <- df[!grepl("Apca",df$Species),]
+        df <- df[!grepl("Brin",df$Species),]
+        df <- df[!grepl("Juni",df$Species),]
+        df <- df[!grepl("Plla",df$Species),]
+        df <- df[!grepl("Romu",df$Species),]
+        df <- df[!grepl("Stme",df$Species),]
+        df <- df[!grepl("Umsp",df$Species),]
+        df <- df[!grepl("Ulsp",df$Species),]
+        df <- df[!grepl("Spsp",df$Species),]
+        df <- df[!grepl("Acru",df$Species),]
+        df <- df[!grepl("Oebi",df$Species),]
+        df <- df[!grepl("Besp",df$Species),]
+        df <- df[!grepl("Prse",df$Species),]
+        df <- df[!grepl("Quru",df$Species),]
+        df <- df[!grepl("Prpe",df$Species),]
+        df <- df[!grepl("Veth",df$Species),]
+        df <- df[!grepl("Anma",df$Species),]
+        df <- df[!grepl("Vaan",df$Species),]
+        df <- df[!grepl("Gnul",df$Species),]
+        df <- df[!grepl("Trdu",df$Species),]
+        df <- df[!grepl("Alpe",df$Species),]
+        df <- df[!grepl("Sypi",df$Species),]
+        df <- df[!grepl("Hipi",df$Species),]
+        df <- df[!grepl("Popsp",df$Species),]
+        return(df)
+}
+
 # change species names
 change_spp <- function(df){
   df$Species[df$Species == "Rubsp"] <- "Rusp"
   df$Species[df$Species == "Rufl"] <- "Rusp"
+  df$Species[df$Species == "Rual"] <- "Rusp"
   df$Species[df$Species == "Hica"] <- "Hisp"
-  df$Species[df$Species == "Sosp"] <- "Spsp"
+  df$Species[df$Species == "Assy"] <- "Assp"
+  df$Species[df$Species == "Poco"] <- "Posp"
+  df$Species[df$Species == "popr"] <- "Popr"
+  df$Species[df$Species == "Popr"] <- "Posp"
+  df$Species[df$Species == "Trre"] <- "Trsp"
+  df$Species[df$Species == "Trpr"] <- "Trsp"
+  df$Species[df$Species == "Sogi"] <- "Sosp"
+  df$Species[df$Species == "Sone"] <- "Sosp"
+  df$Species[df$Species == "Sohi"] <- "Sosp"
   df$Species[df$Species == "Syal"] <- "Syla"
   df$Species[df$Species == "phpr"] <- "Phpr"
   df$Species[df$Species == "Elrre"] <- "Elre"
-  df$Species[df$Species == "popr"] <- "Popr"
   df$Species[df$Species == "Lepidium campestre"] <- "Leca"
   df$Species[df$Species == "Ramu"] <- "Romu"
   df$Species[df$Species == "Anspp"] <- "Ansp"
