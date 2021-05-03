d160c684aee9dde1100c9243e119c2233cb93f86
Kara Dobson <dobsonk2@msu.edu>
2021-05-03 09:42:35 -0400

    updated ZINB models

--- scripts/herbivory_scripts/herbivory_analyses.R
+++ scripts/herbivory_scripts/herbivory_analyses.R
@@ -11,7 +11,6 @@
 # Is herbivory different between warmed and ambient treatments? Hypothesis: Ambient plants will have higher herbivory
   # only run analyses on plots with no insecticide
   # include year as a treatment (is this difference seen each year)?
-  # species + plot as random effects?
 # Is herbivory different between warmed/ambient for native vs exotic? same for growth habit
 
 
@@ -46,18 +45,29 @@ herb$year[herb$year == 2019] <- 5
 herb$year[herb$year == 2020] <- 6
 
 # Remove NAs
-!complete.cases(herb)
 herb <- herb[complete.cases(herb),]
 
 # create dataframes for kbs and umbs only for plots with no insecticide
 herb_kbs <- subset(herb, site == "kbs" & insecticide == "insects")
 herb_umbs <- subset(herb, site == "umbs" & insecticide == "insects")
 
+# only keep species that were recorded in both warmed and ambient plots
+# models below won't run if species are only recorded in either warmed or ambient plots
+herb_kbs <- herb_kbs %>%
+        group_by(species) %>% 
+        filter(all(c('warmed', 'ambient') %in% state))
+herb_umbs <- herb_umbs %>%
+        group_by(species) %>% 
+        filter(all(c('warmed', 'ambient') %in% state))
+
 # How much of the data is zeros?
 100*sum(herb_kbs$p_eaten == 0)/nrow(herb_kbs) #68% - thats a lot! probably have to use a zero-inflated model,
 # but I'll still check for normality & try some transformations below
-100*sum(herb_umbs$p_eaten == 0)/nrow(herb_umbs) #59%
+100*sum(herb_umbs$p_eaten == 0)/nrow(herb_umbs) #60%
 
+# checking to see if any species/state combos are all zeros
+with(herb_kbs,table(species,state,p_eaten==0)) 
+with(herb_umbs,table(species,state,p_eaten==0)) # yes for both
 
 
 ####### kbs #########
@@ -71,7 +81,6 @@ qqPlot(fit)
 hist(herb_kbs$p_eaten[herb_kbs$state == "ambient"])
 hist(herb_kbs$p_eaten[herb_kbs$state == "warmed"])
 # not normal, attempting to transform data below
-
 # log transform
 herb_kbs$p_log <- log(herb_kbs$p_eaten+1)
 hist(herb_kbs$p_log)
@@ -100,30 +109,43 @@ herb_kbs %>%
 # zero-inflated negative binomial
 # is state the variable that predicts the excess zeros?
 # this is probably the right one since the 0's in the data are real counts
-m.neg1 <- zeroinfl(p_eaten ~ state | state,
+m1 <- zeroinfl(p_eaten ~ state,
                dist = 'negbin',
                data = herb_kbs)
-summary(m.neg1)
+summary(m1)
 
-# is species the variable that predicts excess zeros?
-m.neg2 <- zeroinfl(p_eaten ~ state | species,
+# interaction between state and species
+m2 <- zeroinfl(p_eaten ~ state * species,
                    dist = 'negbin',
                    data = herb_kbs)
-summary(m.neg2)
+summary(m2) # NaNs produced due to complete separation
+
+# state and species as separate fixed effects
+m3 <- zeroinfl(p_eaten ~ state + species,
+                     dist = 'negbin',
+                     data = herb_kbs)
+summary(m3) # NaNs produced due to complete separation
+
+# is species the variable that predicts excess zeros?
+m4 <- zeroinfl(p_eaten ~ state | species,
+                   dist = 'negbin',
+                   data = herb_kbs)
+summary(m4)
+
+# likelihood ratio test
+lrtest(m1, m2, m3, m4) # m2 has the highest loglik, but m3 may be better
 
 # check dispersion
-E <- resid(m.neg1, type = "pearson")
+E <- resid(m2, type = "pearson")
 N  <- nrow(herb_kbs)
-p  <- length(coef(m.neg1)) + 1 # '+1' is due to theta
+p  <- length(coef(m2)) + 1 # '+1' is due to theta
 sum(E^2) / (N - p) # pretty close to one, not bad
 
-E2 <- resid(m.neg2, type = "pearson")
+E2 <- resid(m3, type = "pearson")
 N2 <- nrow(herb_kbs)
-p2  <- length(coef(m.neg2)) + 1 
-sum(E2^2) / (N2 - p2) # not as good as the first one
+p2  <- length(coef(m3)) + 1 
+sum(E2^2) / (N2 - p2) # almost the same as the prior model, a little worse
 
-# comparing the two models with a likelihood ratio test
-lrtest(m.neg1, m.neg2) #m.neg2 is better, but m.neg1 makes more sense for the data
 
 
 
@@ -156,37 +178,49 @@ herb_umbs %>%
 # zero-inflated negative binomial
 # is state the variable that predicts the excess zeros?
 # this is probably the right one since the 0's in the data are real counts
-m.neg3 <- zeroinfl(p_eaten ~ state | state,
+m5 <- zeroinfl(p_eaten ~ state | state,
                    dist = 'negbin',
                    data = herb_umbs)
-summary(m.neg3)
+summary(m5)
+
+# interaction between state and species
+m6 <- zeroinfl(p_eaten ~ state * species,
+               dist = 'negbin',
+               data = herb_umbs)
+summary(m6) # NaNs produced due to complete separation
+
+# state and species as separate fixed effects
+m7 <- zeroinfl(p_eaten ~ state + species,
+               dist = 'negbin',
+               data = herb_umbs)
+summary(m7) # NaNs produced due to complete separation
 
 # is species the variable that predicts excess zeros?
-m.neg4 <- zeroinfl(p_eaten ~ state | species,
+m8 <- zeroinfl(p_eaten ~ state | species,
                    dist = 'negbin',
                    data = herb_umbs)
-summary(m.neg4)
+summary(m8)
+
+# likelihood ratio test
+lrtest(m5, m6, m7, m8) # m6 has the highest loglik, but m7 may be better
 
 # check dispersion
-E3 <- resid(m.neg3, type = "pearson")
+E3 <- resid(m6, type = "pearson")
 N3  <- nrow(herb_umbs)
-p3  <- length(coef(m.neg3)) + 1 # '+1' is due to theta
-sum(E3^2) / (N3 - p3) # a little low
+p3  <- length(coef(m6)) + 1 # '+1' is due to theta
+sum(E3^2) / (N3 - p3) # pretty close to one
 
-E4 <- resid(m.neg4, type = "pearson")
+E4 <- resid(m7, type = "pearson")
 N4 <- nrow(herb_umbs)
-p4  <- length(coef(m.neg4)) + 1 
-sum(E4^2) / (N4 - p4) # pretty close to one
-
-# comparing the two models with a likelihood ratio test
-lrtest(m.neg3, m.neg4) #m.neg4 is better, but m.neg3 makes more sense for the data
+p4  <- length(coef(m7)) + 1 
+sum(E4^2) / (N4 - p4) # closer than the prior model
 
 
 
 
 
+# old code from kileighs analyses
 ###### running analyses ########
-## partially taken from kileigh's old code ##
 moda <- lmer(p_eaten ~ state*year + (1|species) + (1|plot), herb_kbs)
 modb <- lmer(p_eaten ~ state + year + (1|species) + (1|plot), herb_kbs)
 modc <- lmer(p_eaten ~ state + (1|year) + (1|species) + (1|plot), herb_kbs)
