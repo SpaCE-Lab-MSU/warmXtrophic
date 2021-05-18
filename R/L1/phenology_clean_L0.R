8f0e903c9c94d14cc260fabb077a929422b14d4a
moriahy <yomoriah@gmail.com>
2021-05-18 10:39:20 -0400

    copying files over to R folder

---
+++ R/L1/phenology_clean_L0.R
@@ -0,0 +1,267 @@
+# TITLE:          Phenology Data Cleanup
+# AUTHORS:        Moriah Young
+# COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Pat Bills, Kara Dobson
+# DATA INPUT:     Data imported as csv files from shared Google drive L0 folder - all phenology data
+#                 for each site and every year 
+# DATA OUTPUT:    A csv file containing phenology data for all sites & years is uploaded
+#                 to the L1 plant comp folder
+# PROJECT:        warmXtrophic
+# DATE:           December, 2020
+
+# Clear all existing data
+rm(list=ls())
+
+#Load packages
+library(tidyverse)
+
+# Set working directory to Google Drive
+# Pat said he doesn't setwd --> figure out .renviron
+setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
+
+# Source in needed functions from phenology_functions.R
+source("/Users/moriahyoung/Documents/GitHub/warmXtrophic/scripts/phenology_scripts/phenology_functions.R")
+
+### Read in meta data files
+# Read in species list
+taxa <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L0/taxon.csv", stringsAsFactors=F)
+# Change column name for from "code" to "Species" to match cleaned phenology data
+colnames(taxa) <- sub("code", "species", colnames(taxa))
+
+# Read in plot list
+plot_info <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L0/plot.csv")
+
+# Read in phenology data
+kbs_2015 <- read.csv("L0/KBS/2015/kbs_flwr_sd_2015.csv")
+kbs_2016 <- read.csv("L0/KBS/2016/kbs_flwr_sd_2016.csv")
+kbs_2017 <- read.csv("L0/KBS/2017/kbs_flwr_sd_2017.csv")
+kbs_2018 <- read.csv("L0/KBS/2018/kbs_flwr_sd_2018.csv")
+kbs_2019 <- read.csv("L0/KBS/2019/kbs_flwr_sd_2019.csv")
+kbs_2020 <- read.csv("L0/KBS/2020/kbs_flwr_sd_2020.csv")
+umbs_2015 <- read.csv("L0/UMBS/2015/umbs_flwr_sd_2015.csv")
+umbs_2016 <- read.csv("L0/UMBS/2016/umbs_flwr_sd_2016.csv")
+umbs_2017 <- read.csv("L0/UMBS/2017/umbs_flwr_sd_2017.csv")
+umbs_2018 <- read.csv("L0/UMBS/2018/umbs_flwr_sd_2018.csv")
+umbs_2019 <- read.csv("L0/UMBS/2019/umbs_flwr_sd_2019.csv")
+umbs_2020 <- read.csv("L0/UMBS/2020/umbs_flwr_sd_2020.csv")
+
+# Add "Site" column for kbs and umbs 2016
+kbs_2016$Site <- "kbs"
+umbs_2016$Site <- "umbs"
+
+# Change column name for kbs 2015
+colnames(kbs_2015) <- sub("event", "Action", colnames(kbs_2015))
+colnames(umbs_2015) <- sub("event", "Action", colnames(umbs_2015))
+
+# remove species from UMBS (and not KBS)
+#remove_spp <- function(df, site, species){
+#        vec <- 
+#        df = df[!grepl("species",df$Species),]
+#        return(df)
+#}
+
+umbs_2016 <- umbs_2016[!grepl("Vear",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Vear",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Vear",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Vear",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Vear",umbs_2020$Species),]
+
+umbs_2016 <- umbs_2016[!grepl("Elre",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Elre",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Elre",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Elre",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Elre",umbs_2020$Species),]
+
+umbs_2016 <- umbs_2016[!grepl("Rusp",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Rusp",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Rusp",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Rusp",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Rusp",umbs_2020$Species),]
+
+umbs_2016 <- umbs_2016[!grepl("Ruag",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Ruag",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Ruag",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Ruag",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Ruag",umbs_2020$Species),]
+
+umbs_2016 <- umbs_2016[!grepl("Rual",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Rual",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Rual",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Rual",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Rual",umbs_2020$Species),]
+
+# Add dataframes into a list so that needed functions can be applied 
+phen_list <- list(kbs_2015=kbs_2015, kbs_2016=kbs_2016, kbs_2017=kbs_2017, kbs_2018=kbs_2018, kbs_2019=kbs_2019, kbs_2020=kbs_2020, 
+                  umbs_2015=umbs_2015, umbs_2016=umbs_2016, umbs_2017=umbs_2017, umbs_2018=umbs_2018, umbs_2019=umbs_2019, umbs_2020=umbs_2020)
+
+# Apply functions
+phen_list <- lapply(phen_list, change_colnames) 
+phen_list <- lapply(phen_list, remove_col, name=c("Julian", "Notes", "collector", "julian", "notes"))
+phen_list <- lapply(phen_list, change_date)
+phen_list <- lapply(phen_list, change_plotID)
+
+# Look over the contents of species, site, and dates after applying functions
+lapply(phen_list, spp_name) # look over species code to see what needs to be fixed
+lapply(phen_list, site_name) # need to make these all the same for each site
+lapply(phen_list, date_check) # see if any dates were entered incorrectly, like the year
+lapply(phen_list, plot_check) # see if there are any repeat plot IDs
+
+# Fixing species names
+phen_list <- lapply(phen_list, change_spp)
+lapply(phen_list, spp_name) # looks good
+
+# Would be great to write some code where you can compare the species codes in the data with
+# our species list
+#spp_name <- function(df){
+#        spp <- unique(sort(df[["Species"]]))
+#        return(spp)
+#}
+#unique(dat$Species)
+#setdiff(unique(dat$Species), unique(taxa$code))
+
+# Removing species
+phen_list <- lapply(phen_list, remove_spp)
+lapply(phen_list, spp_name) 
+
+# Combining some species codes
+phen_list <- lapply(phen_list, change_spp_2)
+lapply(phen_list, spp_name)
+
+# Fixing site names
+phen_list <- lapply(phen_list, change_site)
+lapply(phen_list, site_name) # looks good
+
+# Fixing site dates
+phen_list <- lapply(phen_list, change_date)
+lapply(phen_list, date_check) # looks good
+
+# Merge final data (make a single data frame from all of the lists)
+phen_merge <- rbind(phen_list$kbs_2015, phen_list$kbs_2016, phen_list$kbs_2017, phen_list$kbs_2018, phen_list$kbs_2019, phen_list$kbs_2020,
+                    phen_list$umbs_2016, phen_list$umbs_2017, phen_list$umbs_2018, phen_list$umbs_2019, phen_list$umbs_2020)
+str(phen_merge)
+sort(unique(phen_merge$Species))
+
+# Change column names to lowercase
+names(phen_merge) <- tolower(names(phen_merge))
+
+# Fix date column & add column for the year, month, and julian day
+phen_merge$date <- as.Date(phen_merge$date, format="%m/%d/%Y")
+phen_merge$year <- format(phen_merge$date,format="%Y")
+phen_merge$month <- format(phen_merge$date,format="%m")
+phen_merge$julian <- format(phen_merge$date, "%j")
+head(phen_merge)
+str(phen_merge)
+
+# Make julian column numeric
+phen_merge$julian <- as.numeric(phen_merge$julian)
+str(phen_merge)
+
+# Merge meta data with phenology data - merge cleaned data with the plot and species level information
+phen_merge2 <- merge(phen_merge, plot_info, by = "plot")
+
+sort(unique(phen_merge$plot))
+phen_merge2 <- left_join(plot_info, phen_merge, by = "plot")
+sort(unique(phen_merge2$species))
+
+# taxon contains "site" which is the site where the species is found on our meta-data table, but those data exist in our plant_comp_merge dataset already.
+# Delete "site" from taxon so it doesn't accidentally get merged in.
+taxa$site<-NULL
+
+# merge taxon meta data with the phenology data
+phen_data <- left_join(phen_merge2, taxa, by = "species")
+sort(unique(phen_data$species))
+
+# remove unnecessary columns
+phen_data$old_code <- NULL
+phen_data$old_name <- NULL
+phen_data$resolution <- NULL
+phen_data$old_species <- NULL
+
+# Change column names to lowercase (again)
+names(phen_data) <- tolower(names(phen_data))
+str(phen_data)
+
+phen_data <- phen_data[, c("site", "plot", "species", "action", "date", "julian", "year", "month", "treatment_key", "state",
+                          "insecticide", "scientific_name", "common_name", "usda_species", "lter_species",
+                          "origin", "group", "family", "duration", "growth_habit")]
+
+str(phen_data)
+
+# make a column that breaks down years as 1, 2, 3, 4, 5, 6 and into factors
+phen_data$year_factor <- 
+        ifelse(phen_data$year == 2015, "1",
+               ifelse(phen_data$year == 2016, "2",
+                      ifelse(phen_data$year == 2017, "3",
+                             ifelse(phen_data$year == 2018, "4",
+                                    ifelse(phen_data$year == 2019, "5",
+                                           ifelse(phen_data$year == 2020, "6", NA))))))
+
+# Order warm and ambient so that warm shows up first in plotting (and is default is red = warm; blue = ambient). First make it a factor
+phen_data$state <- as.factor(phen_data$state)
+levels(phen_data$state)
+# [1] "ambient" "warmed" 
+phen_data$state <- factor(phen_data$state, levels(phen_data$state)[c(2,1)])
+levels(phen_data$state)
+# [1] "warmed"  "ambient"
+
+# write a new csv with the cleaned and merge data and upload to the shared google drive
+write.csv(phen_data, file="L1/phenology/final_flwr_sd_L1.csv")
+
+# Create separate data frames for flowering and seeding
+phen_flwr <- subset(phen_data, action == "flower")
+phen_sd <- subset(phen_data, action == "seed")
+
+#### FLOWERING ####
+### Create a data frame at the SPECIES LEVEL that includes median date of flower and first flower date
+# First Flower by SPECIES LEVEL - filter data to contain the date of first flower for each species at each plot
+FirstFlwr_spp <- phen_flwr %>%
+        group_by(plot, year, species, state, site, action, origin, insecticide, treatment_key, year_factor) %>%
+        summarize(julian_min = min(julian, na.rm=T))
+
+# Median Flower Date by sPECIES LEVEL - filter data to contain the median date of flower for each species at each plot
+MedianFlwr_spp <- phen_flwr %>%
+        group_by(plot, year, species, state, site, action, origin, insecticide, treatment_key, year_factor) %>%
+        summarize(julian_median = median(julian, na.rm=T))
+
+# Merge the two data frames above so that you have one data frame that includes median date of flower and first date
+# of flower at SPECIES LEVEL
+phen_flwr_spp <- merge(FirstFlwr_spp, MedianFlwr_spp)
+
+# write a new csv with flowering data at the SPECIES LEVEL and upload to the shared google drive
+write.csv(phen_flwr_spp, file="L1/phenology/final_flwr_species_L1.csv")
+
+### Create a data frame at the PLOT LEVEL that includes median date of flower and first flower
+# First Flower Date by PLOT LEVEL
+FirstFlwr_plot <- phen_flwr %>%
+        group_by(plot, year, state, site, action, insecticide, treatment_key, year_factor) %>%
+        summarize(julian_min = min(julian, na.rm=T))
+
+# Median Flower Date by PLOT LEVEL
+MedianFlwr_plot <- phen_flwr %>%
+        group_by(plot, year, state, site, action, insecticide, treatment_key, year_factor) %>%
+        summarize(julian_median = median(julian, na.rm=T))
+
+# Merge the two data frames above so that you have one data frame that includes median date of flower and first date
+# of flower at PLOT LEVEL
+phen_flwr_plot <- merge(FirstFlwr_plot, MedianFlwr_plot)
+
+# write a new csv with flowering data at the PLOT LEVEL and upload to the shared google drive
+write.csv(phen_flwr_plot, file="L1/phenology/final_flwr_plot_L1.csv")
+
+#### SEED SET ####
+### Create a data frame at the SPECIES LEVEL that includes first date of seed
+# First Seed by SPECIES LEVEL - filter data to contain the date of first seed for each species at each plot
+FirstSd_spp <- phen_sd %>%
+        group_by(plot, year, species, state, site, action, origin, insecticide, treatment_key, year_factor) %>%
+        summarize(julian_min = min(julian, na.rm=T))
+
+# write a new csv with first seed date at the SPECIES LEVEL and upload to the shared google drive
+write.csv(FirstSd_spp, file="L1/phenology/final_sd_species_L1.csv")
+
+### Create a data frame at the PLOT LEVEL that includes first date of seed
+# First Flower Date by PLOT LEVEL
+FirstSd_plot <- phen_sd %>%
+        group_by(plot, year, state, site, action, insecticide, treatment_key, year_factor) %>%
+        summarize(julian_min = min(julian, na.rm=T))
+
+# write a new csv with first seed date at the PLOT LEVEL and upload to the shared google drive
+write.csv(FirstSd_plot, file="L1/phenology/final_sd_plot_L1.csv")
---
+++ R/L2/phenology_plots.R
@@ -0,0 +1,302 @@
+# TITLE:          Phenology Data Visualization
+# AUTHORS:        Moriah Young
+# COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Pat Bills, Kara Dobson
+# DATA INPUT:     Data imported as csv files from shared Google drive L1 folder
+# DATA OUTPUT:    
+# PROJECT:        warmXtrophic
+# DATE:           January, 2020
+
+# Clear all existing data
+rm(list=ls())
+
+#Load packages
+library(tidyverse)
+library(plotrix)
+library(cowplot)
+library(ggpubr)
+
+# Set working directory to Google Drive
+setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
+
+############################### Read in data #################################3
+# cleaned phenology data from L1
+phen_data <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L1/phenology/final_flw_sd_L1.csv", stringsAsFactors=FALSE)
+phen_data <- phen_data %>% 
+        select(-X) # get rid of "X" column that shows up
+View(phen_data) # take a look at the data to see if looks good
+
+# Create separate data frames for flowering and seeding
+phen_flwr <- subset(phen_data, action == "flower")
+phen_sd <- subset(phen_data, action == "seed")
+
+###################### Flowering ##########################################
+
+#### Median Flower ####
+MedFlwr_all <- phen_flwr %>%
+        group_by(plot, year, species, state, site, action, origin) %>%
+        summarize(julian = median(julian, na.rm=T))
+
+sum_MedFlwr <- MedFlwr_all %>%
+        group_by(site, state, year) %>%
+        summarize(med_julian = median(julian, na.rm=TRUE),
+                  se = std.error(julian, na.rm = TRUE))
+                  
+sum_MedFlwr_plot <- function(loc) { 
+        MedFlwr_sub <- subset(sum_MedFlwr, site == loc)
+        return(ggplot(MedFlwr_sub, aes(x = state, y = med_julian, fill = state)) +
+                       facet_grid(.~year) +
+                       geom_bar(position = "identity", stat = "identity", color = "black") +
+                       geom_errorbar(aes(ymin = med_julian - se, ymax = med_julian + se), width = 0.2,
+                                     position = "identity") +
+                       labs(x = NULL, y = NULL, title = loc) +
+                       scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+                       scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+                       coord_cartesian(ylim = c(150, NA)) +
+                       theme(legend.position = "none") +
+                       theme_classic())
+}
+
+sum_MedFlwr_plot("kbs")
+sum_MedFlwr_plot("umbs")
+
+# Copying Kara but doing it for phenology
+all_Med_u <- sum_MedFlwr_plot("umbs")
+all_Med_k <- sum_MedFlwr_plot("kbs")
+
+final_Med_all <- ggarrange(all_Med_k, all_Med_u, nrow = 2, legend = "none")
+annotate_figure(final_Med_all,
+                left = text_grob("Julian Day of Median Flower", color = "black", rot = 90),
+                bottom = text_grob("State", color = "black"))
+
+
+#### First Flower #####
+
+# Filter data to contain the date of first flower for each species at each plot
+FirstFlower_all <- phen_flwr %>%
+        group_by(plot, year, species, state, site, action, origin) %>%
+        summarize(julian = min(julian, na.rm=T))
+
+# Filter FirstFlower_all data set to calculate the mean and sd for julian date of first flower for each species at ambient
+# and warmed plots by YEAR
+sum_FirstFlower <- FirstFlower_all %>%
+        group_by(site, state, species, year) %>%
+        summarize(avg_julian = mean(julian, na.rm = TRUE),
+                  se = std.error(julian, na.rm = TRUE))
+
+# This creates function so that you can look at a specific species at either kbs or umbs and it's mean julian day of first flower for every year
+# of data collection
+FirstFlower_plot <- function(spp, loc) { 
+        FirstFlower_spp <- subset(sum_FirstFlower, species == spp & site == loc)
+        return(ggplot(FirstFlower_spp, aes(x = state, y = avg_julian, fill = state)) +
+                       facet_grid(.~year) +
+                       geom_bar(position = "identity", stat = "identity", color = "black") +
+                       geom_errorbar(aes(ymin = avg_julian - se, ymax = avg_julian + se), width = 0.2,
+                                     position = "identity") +
+                       labs(x = "State", y = "Julian Date", title = spp, subtitle = loc) +
+                       coord_cartesian(ylim = c(120, NA)) +
+                       scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+                       scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+                       theme_classic())
+}
+
+FirstFlower_plot("Popr", "umbs")
+FirstFlower_plot("Popr", "kbs")
+
+# This filters the FirstFlower_all data set to calculate the average julian day of first flower combining all species for each site 
+# and year for warmed and ambient
+sum_FirstFlwr_state <- FirstFlower_all %>%
+        group_by(site, state, year) %>%
+        summarize(avg_julian = mean(julian, na.rm = TRUE),
+                  se = std.error(julian, na.rm = TRUE)) 
+
+# This creates a function that returns plots for a given site and year for mean julian day of first flower comparing ambient vs warmed plots
+sum_FirstFlwr_plot <- function(loc) { 
+        FirstFlwr_sub <- subset(sum_FirstFlwr_state, site == loc)
+        return(ggplot(FirstFlwr_sub, aes(x = state, y = avg_julian, fill = state)) +
+                       facet_grid(.~year) +
+                       geom_bar(position = "identity", stat = "identity", color = "black") +
+                       geom_errorbar(aes(ymin = avg_julian - se, ymax = avg_julian + se), width = 0.2,
+                                     position = "identity") +
+                       labs(x = NULL, y = NULL, title = loc) +
+                       scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+                       scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+                       coord_cartesian(ylim = c(150, NA)) +
+                       theme(legend.position = "none") +
+                       theme_classic())
+}
+
+sum_FirstFlwr_plot("kbs")
+sum_FirstFlwr_plot("umbs")
+
+# Copying Kara but doing it for phenology
+all_u <- sum_FirstFlwr_plot("umbs")
+all_k <- sum_FirstFlwr_plot("kbs")
+
+final_all <- ggarrange(all_k, all_u, nrow = 2, legend = "none")
+annotate_figure(final_all,
+                left = text_grob("Julian Day of First Flower", color = "black", rot = 90),
+                bottom = text_grob("State", color = "black"))
+
+# boxplot (again, taking from Kara)
+firstflwr_plot_box <- function(loc) { 
+        phen_spp <- subset(FirstFlower_all, site == loc)
+        return(ggplot(phen_spp, aes(x = state, y = julian, fill = state)) +
+                       #facet_grid(.~year) +
+                       geom_boxplot(color = "black") +
+                       labs(x = NULL, y = NULL, title = loc) +
+                       scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+                       scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+                       coord_cartesian(ylim = c(0, 300)) +
+                       theme(legend.position = "none") +
+                       geom_jitter(shape=16, position=position_jitter(0.2)) +
+                       theme_classic())
+}
+all_u <- firstflwr_plot_box("umbs")
+all_k <- firstflwr_plot_box("kbs")
+
+all_u #idk this looks weird
+all_k
+
+# We want to see the graphs for kbs and umbs side by side, so using "cowplot" to do this
+#kbs <- sum_FirstFlwr_plot("kbs")
+#umbs <- sum_FirstFlwr_plot("umbs")
+
+#plot_grid(kbs, umbs,
+#         ncol = 2, nrow = 1)
+
+# Filter FirstFlower_all by plant origin (native/exotic)
+sum_flwr_org <- FirstFlower_all %>%
+        group_by(site, origin, state) %>%
+        summarize(avg_julian = mean(julian, na.rm = TRUE),
+                  se = std.error(julian, na.rm = TRUE))
+sum_flwr_org <- subset(sum_flwr_org, origin == "Exotic" | origin == "Native")
+View(sum_flwr_org)
+
+flwr_org_plot <- function(loc) { 
+        flwr_spp <- subset(sum_flwr_org, site == loc)
+        return(ggplot(flwr_spp, aes(x = origin, y = avg_julian, fill = state)) +
+                       #facet_grid(.~year) +
+                       geom_bar(position = "dodge", stat = "identity", color = "black") +
+                       geom_errorbar(aes(ymin = avg_julian - se, ymax = avg_julian + se), width = 0.2,
+                                     position = position_dodge(0.9)) +
+                       labs(x = NULL, y = NULL, title = loc) +
+                       scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+                       scale_x_discrete(labels=c("Exotic" = "E", "Native" = "N")) +
+                       coord_cartesian(ylim = c(100, NA)) +
+                       theme_grey())
+}
+
+orgin_u <- flwr_org_plot("umbs")
+orgin_k <- flwr_org_plot("kbs")
+
+final_orgin <- ggarrange(orgin_k, orgin_u, ncol = 2, legend = "none")
+annotate_figure(final_orgin,
+                left = text_grob("Julian Day of First Flower", color = "black", rot = 90),
+                bottom = text_grob("Origin", color = "black"))
+
+##### Flowering Duration #####
+
+# Filter data to calculate the duration of flowering time for each species at every plot and year
+flwr_duration <- phen_flwr %>% 
+        group_by(site, plot, species, year, state, action, origin) %>%
+        summarise(flwr_duration = max(julian) - min(julian)) 
+#summarise(result = diff(range(julian)))
+
+# Filter flwr_duration data frame to calculate the mean duration by native vs exotic 
+sum_flwrduration_org <- flwr_duration %>% 
+        group_by(site, state, action, origin) %>% 
+        summarise(mean_duration = mean(flwr_duration))
+View(sum_flwrduration_org) 
+
+# Filter flwr_duration data set by state and year and calculate mean duration of flowering
+sum_flwrduration_state <- flwr_duration %>% 
+        group_by(site, state, action, year) %>% 
+        summarise(mean_duration = mean(flwr_duration),
+                  se = std.error(flwr_duration, na.rm = TRUE))
+
+sum_FlwrDurState_plot <- function(loc) { 
+        flwr_Duration <- subset(sum_flwrduration_state, site == loc)
+        return(ggplot(flwr_Duration, aes(x = state, y = mean_duration, fill = state)) +
+                       facet_grid(.~year) +
+                       geom_bar(position = "identity", stat = "identity", color = "black") +
+                       geom_errorbar(aes(ymin = mean_duration - se, ymax = mean_duration + se), width = 0.2,
+                                     position = "identity") +
+                       labs(x = NULL, y = NULL, title = loc) +
+                       coord_cartesian(ylim = c(0, NA)) +
+                       scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+                       scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+                       theme_classic())
+}
+
+all_k_dur <- sum_FlwrDurState_plot("kbs")
+all_u_dur <- sum_FlwrDurState_plot("umbs")
+
+final_all_dur <- ggarrange(all_k_dur, all_u_dur, nrow = 2, legend = "none")
+annotate_figure(final_all_dur,
+                left = text_grob("Julian Day of Mean Flower Duration", color = "black", rot = 90),
+                bottom = text_grob("State", color = "black"))
+
+##################### Seeding #########################
+
+# Filter data to contain the date of first flower for each species at each site and year
+FirstSeed_all <- phen_sd %>%
+        group_by(plot, year, species, state, site, action) %>%
+        summarize(julian = min(julian, na.rm=T))
+
+# Filter FirstSeed_all data set to calculate the mean and sd for julian date of first seed for each species at ambient
+# and warmed plots by YEAR        
+sum_FirstSeed <- FirstSeed_all %>%
+        group_by(site, state, species, year) %>%
+        summarize(avg_julian = mean(julian, na.rm = TRUE),
+                  se = std.error(julian, na.rm = TRUE))
+
+# This filters the FirstSeed_all data set to calculate the average julian day of first seed combining all species for each site 
+# and year for warmed and ambient
+sum_FirstSeed_state <- FirstSeed_all %>%
+        group_by(site, state, year) %>%
+        summarize(avg_julian = mean(julian, na.rm = TRUE),
+                  se = std.error(julian, na.rm = TRUE)) 
+
+# This creates function so that you can look at a specific species at either kbs or umbs and it's mean julian day of first seed for every year
+# of data collection
+FirstSeed_plot <- function(spp, loc) { 
+        FirstSeed_spp <- subset(sum_FirstSeed, species == spp & site == loc)
+        return(ggplot(FirstSeed_spp, aes(x = state, y = avg_julian, fill = state)) +
+                       facet_grid(.~year) +
+                       geom_bar(position = "identity", stat = "identity") +
+                       geom_errorbar(aes(ymin = avg_julian - se, ymax = avg_julian + se), width = 0.2,
+                                     position = "identity") +
+                       labs(x = "State", y = "Julian Date", title = spp, subtitle = loc) +
+                       #coord_cartesian(ylim = c(150, 300)) +
+                       scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+                       scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+                       theme_grey())
+}
+
+FirstSeed_plot("Popr", "umbs")
+FirstSeed_plot("Popr", "kbs")
+
+# This creates a function that returns plots for a given site and all years for mean julien day of first seed 
+# comparing ambient vs warmed plots
+sum_FirstSeed_plot <- function(loc) { 
+        FirstSeed_sub <- subset(sum_FirstSeed_state, site == loc)
+        return(ggplot(FirstSeed_sub, aes(x = state, y = avg_julian, fill = state)) +
+                       facet_grid(.~year) +
+                       geom_bar(position = "identity", stat = "identity", color = "black") +
+                       geom_errorbar(aes(ymin = avg_julian - se, ymax = avg_julian + se), width = 0.2,
+                                     position = "identity") +
+                       labs(x = NULL, y = NULL, title = loc) +
+                       coord_cartesian(ylim = c(170, NA)) +
+                       scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+                       scale_x_discrete(labels=c("ambient" = "A", "warmed" = "W")) +
+                       theme_classic())
+}
+
+all_k_sd <- sum_FirstSeed_plot("kbs")
+all_u_sd <- sum_FirstSeed_plot("umbs")
+
+final_all_sd <- ggarrange(all_k_sd, all_u_sd, nrow = 2, legend = "none")
+annotate_figure(final_all_sd,
+                left = text_grob("Julian Day of First Seed", color = "black", rot = 90),
+                bottom = text_grob("State", color = "black"))
+
