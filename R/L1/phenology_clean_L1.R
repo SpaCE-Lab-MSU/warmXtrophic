1bed95a194a0a9450f599a65aa132190f8155218
moriahy <yomoriah@gmail.com>
2021-05-21 09:38:09 -0400

    updating script to use .Renviron

--- R/L1/phenology_clean_L1.R
+++ R/L1/phenology_clean_L1.R
@@ -27,30 +27,30 @@ L0_dir <- Sys.getenv("L0DIR")
 list.files(L0_dir)
 meta <- read.csv(file.path(L0_dir, "plot.csv"))
 taxon <- read.csv(file.path(L0_dir, "taxon.csv"))
-
+colnames(taxon) <- sub("code", "species", colnames(taxon))
 
 ### Read in meta data files
 # Read in species list
-taxa <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L0/taxon.csv", stringsAsFactors=F)
+#taxa <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L0/taxon.csv", stringsAsFactors=F)
 # Change column name for from "code" to "Species" to match cleaned phenology data
-colnames(taxa) <- sub("code", "species", colnames(taxa))
+#colnames(taxon) <- sub("code", "species", colnames(taxon))
 
 # Read in plot list
-plot_info <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L0/plot.csv")
+#plot_info <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L0/plot.csv")
 
 # Read in phenology data
-kbs_2015 <- read.csv("L0/KBS/2015/kbs_flwr_sd_2015.csv")
-kbs_2016 <- read.csv("L0/KBS/2016/kbs_flwr_sd_2016.csv")
-kbs_2017 <- read.csv("L0/KBS/2017/kbs_flwr_sd_2017.csv")
-kbs_2018 <- read.csv("L0/KBS/2018/kbs_flwr_sd_2018.csv")
-kbs_2019 <- read.csv("L0/KBS/2019/kbs_flwr_sd_2019.csv")
-kbs_2020 <- read.csv("L0/KBS/2020/kbs_flwr_sd_2020.csv")
-umbs_2015 <- read.csv("L0/UMBS/2015/umbs_flwr_sd_2015.csv")
-umbs_2016 <- read.csv("L0/UMBS/2016/umbs_flwr_sd_2016.csv")
-umbs_2017 <- read.csv("L0/UMBS/2017/umbs_flwr_sd_2017.csv")
-umbs_2018 <- read.csv("L0/UMBS/2018/umbs_flwr_sd_2018.csv")
-umbs_2019 <- read.csv("L0/UMBS/2019/umbs_flwr_sd_2019.csv")
-umbs_2020 <- read.csv("L0/UMBS/2020/umbs_flwr_sd_2020.csv")
+kbs_2015 <- read.csv(file.path(L0_dir, "KBS/2015/kbs_flwr_sd_2015.csv"))
+kbs_2016 <- read.csv(file.path(L0_dir, "KBS/2016/kbs_flwr_sd_2016.csv"))
+kbs_2017 <- read.csv(file.path(L0_dir, "KBS/2017/kbs_flwr_sd_2017.csv"))
+kbs_2018 <- read.csv(file.path(L0_dir, "KBS/2018/kbs_flwr_sd_2018.csv"))
+kbs_2019 <- read.csv(file.path(L0_dir, "KBS/2019/kbs_flwr_sd_2019.csv"))
+kbs_2020 <- read.csv(file.path(L0_dir, "KBS/2020/kbs_flwr_sd_2020.csv"))
+umbs_2015 <- read.csv(file.path(L0_dir, "UMBS/2015/umbs_flwr_sd_2015.csv"))
+umbs_2016 <- read.csv(file.path(L0_dir, "UMBS/2016/umbs_flwr_sd_2016.csv"))
+umbs_2017 <- read.csv(file.path(L0_dir, "UMBS/2017/umbs_flwr_sd_2017.csv"))
+umbs_2018 <- read.csv(file.path(L0_dir, "UMBS/2018/umbs_flwr_sd_2018.csv"))
+umbs_2019 <- read.csv(file.path(L0_dir, "UMBS/2019/umbs_flwr_sd_2019.csv"))
+umbs_2020 <- read.csv(file.path(L0_dir, "UMBS/2020/umbs_flwr_sd_2020.csv"))
 
 # Add "Site" column for kbs and umbs 2016
 kbs_2016$Site <- "kbs"
