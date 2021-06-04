f029c424a20416b3b97572973c98b3116f45e1ad
moriahy <yomoriah@gmail.com>
2021-06-04 11:43:01 -0400

    updated clean phenology script with renviron path files

--- R/L1/phenology_clean_L1.R
+++ R/L1/phenology_clean_L1.R
@@ -15,7 +15,7 @@ rm(list=ls())
 library(tidyverse)
 
 # Set working directory to Google Drive
-# Pat said he doesn't setwd --> figure out .renviron
+# Pat said he doesn't setwd
 # setwd("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/")
 
 # Source in needed functions
@@ -24,19 +24,14 @@ source("/Users/moriahyoung/Documents/GitHub/warmXtrophic/R/L1/phenology_function
 # Read in data
 Sys.getenv("L0DIR")
 L0_dir <- Sys.getenv("L0DIR")
+L1_dir <- Sys.getenv("L1DIR")
 list.files(L0_dir)
-meta <- read.csv(file.path(L0_dir, "plot.csv"))
+# read in meta data for plots
+plot_info <- read.csv(file.path(L0_dir, "plot.csv"))
+# read in neta taxon list
 taxon <- read.csv(file.path(L0_dir, "taxon.csv"))
+# change column name for from "code" to "Species" to match cleaned phenology data
 colnames(taxon) <- sub("code", "species", colnames(taxon))
-
-### Read in meta data files
-# Read in species list
-#taxa <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L0/taxon.csv", stringsAsFactors=F)
-# Change column name for from "code" to "Species" to match cleaned phenology data
-#colnames(taxon) <- sub("code", "species", colnames(taxon))
-
-# Read in plot list
-#plot_info <- read.csv("/Volumes/GoogleDrive/Shared drives/SpaCE_Lab_warmXtrophic/data/L0/plot.csv")
 
 # Read in phenology data
 kbs_2015 <- read.csv(file.path(L0_dir, "KBS/2015/kbs_flwr_sd_2015.csv"))
@@ -56,17 +51,11 @@ umbs_2020 <- read.csv(file.path(L0_dir, "UMBS/2020/umbs_flwr_sd_2020.csv"))
 kbs_2016$Site <- "kbs"
 umbs_2016$Site <- "umbs"
 
-# Change column name for kbs 2015
+# Change column name for kbs 2015 from event to action
 colnames(kbs_2015) <- sub("event", "Action", colnames(kbs_2015))
 colnames(umbs_2015) <- sub("event", "Action", colnames(umbs_2015))
 
 # remove species from UMBS (and not KBS)
-#remove_spp <- function(df, site, species){
-#        vec <- 
-#        df = df[!grepl("species",df$Species),]
-#        return(df)
-#}
-
 umbs_2016 <- umbs_2016[!grepl("Vear",umbs_2016$Species),]
 umbs_2017 <- umbs_2017[!grepl("Vear",umbs_2017$Species),]
 umbs_2018 <- umbs_2018[!grepl("Vear",umbs_2018$Species),]
@@ -163,19 +152,18 @@ str(phen_merge)
 phen_merge$julian <- as.numeric(phen_merge$julian)
 str(phen_merge)
 
-# Merge meta data with phenology data - merge cleaned data with the plot and species level information
-phen_merge2 <- merge(phen_merge, plot_info, by = "plot")
+# Merge plot meta data with phenology data - merge cleaned data with the plot and species level information
+# phen_merge2 <- merge(phen_merge, plot_info, by = "plot")
 
-sort(unique(phen_merge$plot))
 phen_merge2 <- left_join(plot_info, phen_merge, by = "plot")
 sort(unique(phen_merge2$species))
 
 # taxon contains "site" which is the site where the species is found on our meta-data table, but those data exist in our plant_comp_merge dataset already.
 # Delete "site" from taxon so it doesn't accidentally get merged in.
-taxa$site<-NULL
+taxon$site<-NULL
 
 # merge taxon meta data with the phenology data
-phen_data <- left_join(phen_merge2, taxa, by = "species")
+phen_data <- left_join(phen_merge2, taxon, by = "species")
 sort(unique(phen_data$species))
 
 # remove unnecessary columns
@@ -212,7 +200,7 @@ levels(phen_data$state)
 # [1] "warmed"  "ambient"
 
 # write a new csv with the cleaned and merge data and upload to the shared google drive
-write.csv(phen_data, file="L1/phenology/final_flwr_sd_L1.csv")
+write.csv(phen_data, file.path(L1_dir, "phenology/final_flwr_sd_L1.csv"))
 
 # Create separate data frames for flowering and seeding
 phen_flwr <- subset(phen_data, action == "flower")
@@ -220,12 +208,13 @@ phen_sd <- subset(phen_data, action == "seed")
 
 #### FLOWERING ####
 ### Create a data frame at the SPECIES LEVEL that includes median date of flower and first flower date
+
 # First Flower by SPECIES LEVEL - filter data to contain the date of first flower for each species at each plot
 FirstFlwr_spp <- phen_flwr %>%
         group_by(plot, year, species, state, site, action, origin, insecticide, treatment_key, year_factor) %>%
         summarize(julian_min = min(julian, na.rm=T))
 
-# Median Flower Date by sPECIES LEVEL - filter data to contain the median date of flower for each species at each plot
+# Median Flower Date by SPECIES LEVEL - filter data to contain the median date of flower for each species at each plot
 MedianFlwr_spp <- phen_flwr %>%
         group_by(plot, year, species, state, site, action, origin, insecticide, treatment_key, year_factor) %>%
         summarize(julian_median = median(julian, na.rm=T))
@@ -235,9 +224,10 @@ MedianFlwr_spp <- phen_flwr %>%
 phen_flwr_spp <- merge(FirstFlwr_spp, MedianFlwr_spp)
 
 # write a new csv with flowering data at the SPECIES LEVEL and upload to the shared google drive
-write.csv(phen_flwr_spp, file="L1/phenology/final_flwr_species_L1.csv")
+write.csv(phen_flwr_spp, file.path(L1_dir, "phenology/final_flwr_species_L1.csv"))
 
 ### Create a data frame at the PLOT LEVEL that includes median date of flower and first flower
+
 # First Flower Date by PLOT LEVEL
 FirstFlwr_plot <- phen_flwr %>%
         group_by(plot, year, state, site, action, insecticide, treatment_key, year_factor) %>%
@@ -253,23 +243,25 @@ MedianFlwr_plot <- phen_flwr %>%
 phen_flwr_plot <- merge(FirstFlwr_plot, MedianFlwr_plot)
 
 # write a new csv with flowering data at the PLOT LEVEL and upload to the shared google drive
-write.csv(phen_flwr_plot, file="L1/phenology/final_flwr_plot_L1.csv")
+write.csv(phen_flwr_plot, file.path(L1_dir, "phenology/final_flwr_plot_L1.csv"))
 
 #### SEED SET ####
 ### Create a data frame at the SPECIES LEVEL that includes first date of seed
+
 # First Seed by SPECIES LEVEL - filter data to contain the date of first seed for each species at each plot
 FirstSd_spp <- phen_sd %>%
         group_by(plot, year, species, state, site, action, origin, insecticide, treatment_key, year_factor) %>%
         summarize(julian_min = min(julian, na.rm=T))
 
 # write a new csv with first seed date at the SPECIES LEVEL and upload to the shared google drive
-write.csv(FirstSd_spp, file="L1/phenology/final_sd_species_L1.csv")
+write.csv(FirstSd_spp, file.path(L1_dir, "phenology/final_sd_species_L1.csv"))
 
 ### Create a data frame at the PLOT LEVEL that includes first date of seed
+
 # First Flower Date by PLOT LEVEL
 FirstSd_plot <- phen_sd %>%
         group_by(plot, year, state, site, action, insecticide, treatment_key, year_factor) %>%
         summarize(julian_min = min(julian, na.rm=T))
 
 # write a new csv with first seed date at the PLOT LEVEL and upload to the shared google drive
-write.csv(FirstSd_plot, file="L1/phenology/final_sd_plot_L1.csv")
+write.csv(FirstSd_plot, file.path(L1_dir, "phenology/final_sd_plot_L1.csv"))
