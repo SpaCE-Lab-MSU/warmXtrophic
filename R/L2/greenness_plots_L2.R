536933b4ab73b4fecce5adc132b8dbf4877e90c1
Kara Dobson <dobsonk2@msu.edu>
2021-07-29 12:18:57 -0400

    updating greenness plots

--- R/L2/greenness_plots_L2.R
+++ R/L2/greenness_plots_L2.R
@@ -18,21 +18,25 @@ L1_dir<-Sys.getenv("L1DIR")
 # Read in data
 green <- read.csv(file.path(L1_dir, "Greenness/greenness_L1.csv"))
 
-# Take individual plant average
+# Take plot average
 green2 <- green %>%
-        group_by(plot, plant_number, state) %>%
+        group_by(plot, state) %>%
         summarize(greenness = mean(greenness, na.rm = TRUE))
 
 # Boxplot
-png("warmx_green.png", units="in", width=5, height=5, res=300)
+png("warmx_green.png", units="in", width=6, height=5, res=300)
 ggplot(green2, aes(x = state, y = greenness, fill=state)) +
-        geom_jitter(shape=16, position=position_jitterdodge(), alpha = 0.6, aes(colour = state)) +
+        #geom_jitter(shape=16, position=position_jitterdodge(), alpha = 0.6, aes(colour = state)) +
         scale_color_manual(values = c("ambient" = "olivedrab", "warmed" = "olivedrab")) +
         geom_boxplot(color = "black") +
-        labs(x = "Treatment", y = "Leaf Greenness") +
+        labs(x = "Treatment", y = "Leaf Greenness", face="bold") +
         scale_fill_manual(values = c("olivedrab", "olivedrab")) +
         scale_x_discrete(labels=c("ambient" = "Ambient",
                                   "warmed" = "Warmed")) +
-        theme_classic() +
+        theme_bw() +
+        theme(text = element_text(size=16)) +
+        theme(axis.title.x = element_text(size=16, face="bold", colour = "black"),    
+        axis.title.y = element_text(size=16, face="bold", colour = "black")) +
         theme(legend.position = "none")
+#ggsave("warmx_green.png", width = 6, height = 5, device='png', dpi=300)
 dev.off()
