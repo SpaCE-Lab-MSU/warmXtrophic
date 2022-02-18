4d1be59cd05e5c8f6abc963d896958c7476b3792
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2022-02-18 14:52:13 -0500

    started biomass analyses

---
+++ R/L2/biomass_analyses_L2.R
@@ -0,0 +1,98 @@
+# TITLE:          warmXtrophic biomass and plant composition canalyses
+# AUTHORS:        Kara Dobson
+# COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Pat Bills, Moriah Young
+# DATA INPUT:     Data imported as csv files from shared Google drive L0 folder
+# DATA OUTPUT:    analyses on biomass data
+# PROJECT:        warmXtrophic
+# DATE:           Feb 2022
+
+# Clear all existing data
+rm(list=ls())
+
+# Load packages
+library(tidyverse)
+library(lmerTest)
+library(car)
+library(bbmle)
+library(sjPlot)
+
+# Set working directory 
+L1_dir <- Sys.getenv("L1DIR")
+
+# load data
+# note: we also have 2020 ancillary biomass (no warming treatment), so only analyzing warming effect with 2021 harvested biomass
+kbs_biomass_21 <- read.csv(file.path(L1_dir, "ANPP/kbs_biomass_2021_L1.csv"))
+umbs_biomass_21 <- read.csv(file.path(L1_dir, "ANPP/umbs_biomass_2021_L1.csv"))
+kbs_biomass_21 <- kbs_biomass_21 %>% dplyr::select(-X) # get rid of "X" column that shows up (could fix this in cleaning script)
+umbs_biomass_21 <- umbs_biomass_21 %>% dplyr::select(-X)
+
+# removing uninformative species names?
+
+
+#########################################
+# KBS
+
+#### Data exploration ###
+hist(kbs_biomass_21$weight_g)
+qqnorm(kbs_biomass_21$weight_g)
+shapiro.test(kbs_biomass_21$weight_g)
+# very right skewed
+
+# histograms for each treatment separately - plot level
+hist(kbs_biomass_21$weight_g[kbs_biomass_21$state == "ambient"])
+hist(kbs_biomass_21$weight_g[kbs_biomass_21$state == "warmed"])
+
+# checking individual species
+hist(kbs_biomass_21$weight_g[kbs_biomass_21$species == "Elre"])
+hist(kbs_biomass_21$weight_g[kbs_biomass_21$species == "Soca"])
+hist(kbs_biomass_21$weight_g[kbs_biomass_21$species == "Popr"])
+# still kinda skewed for some of these
+
+# histograms for each species
+ggplot(data = kbs_biomass_21, aes(x = weight_g, fill=state)) + geom_histogram(alpha=0.5, binwidth=10) + facet_wrap(~species)
+
+# density plot
+ggplot(data = kbs_biomass_21, aes(x = weight_g, fill=state)) + geom_density(alpha=0.5) + theme_minimal()
+
+# leverage plots
+fit_k <- lm(weight_g ~ state, data = kbs_biomass_21)
+outlierTest(fit_k) # two outliers, both Soca weights
+qqPlot(fit_k, main="QQ Plot") 
+hist(fit_k$residuals)
+leveragePlots(fit_k)
+
+fit1_k <- lm(weight_g ~ state + species, data = kbs_biomass_21)
+outlierTest(fit1_k) # three outliers, all Soca
+hist(fit1_k$residuals)
+qqPlot(fit1_k, main="QQ Plot") 
+leveragePlots(fit1_k)
+
+fit2_k <- lm(log(weight_g) ~ state + species, data = kbs_biomass_21)
+outlierTest(fit2_k) # no outliers
+hist(fit2_k$residuals)
+qqPlot(fit2_k, main="QQ Plot") 
+leveragePlots(fit2_k)
+# log transformation looks good
+
+### Data analyses ###
+# data seems normal after accounting for variation in species
+mod1_k <- lmer(log(weight_g) ~ state + species + insecticide + (1|plot), kbs_biomass_21, REML=FALSE)
+mod2_k <- lmer(log(weight_g) ~ state * species + insecticide + (1|plot), kbs_biomass_21, REML=FALSE)
+mod3_k <- lmer(log(weight_g) ~ state + species + (1|plot), kbs_biomass_21, REML=FALSE)
+mod4_k <- lmer(log(weight_g) ~ state * species + (1|plot), kbs_biomass_21, REML=FALSE)
+mod5_k <- lmer(log(weight_g) ~ state + (1|plot/species), kbs_biomass_21, REML=FALSE)
+mod6_k <- lmer(log(weight_g) ~ state + insecticide + species + (1|plot), kbs_biomass_21, REML=FALSE)
+mod7_k <- lmer(log(weight_g) ~ state * insecticide + species + (1|plot), kbs_biomass_21, REML=FALSE)
+mod8_k <- lmer(log(weight_g) ~ state + (1|plot) + (1|species), kbs_biomass_21, REML=FALSE)
+
+anova(mod1_k,mod2_k) # mod 2
+anova(mod2_k,mod3_k) # mod 2
+anova(mod2_k,mod4_k) # mod 4
+anova(mod4_k,mod5_k) # mod 4
+anova(mod4_k,mod6_k) # mod 4
+anova(mod4_k,mod7_k) # mod 4
+anova(mod4_k,mod8_k) # mod 4
+plot_model(mod4_k, sort.est = TRUE)
+AICctab(mod1_k,mod2_k,mod3_k,mod4_k,mod5_k,mod6_k,mod7_k,mod8_k,weights=T)
+
+summary(mod4_k)
