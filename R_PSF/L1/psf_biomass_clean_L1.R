90d2758d2be8a6bf0b120e7f42e2123c03d340fe
moriahy <yomoriah@gmail.com>
2022-03-01 19:50:34 -0500

    starting psf biomass clean, plots, and analyses scripts

---
+++ R_PSF/L1/psf_biomass_clean_L1.R
@@ -0,0 +1,35 @@
+# TITLE: Plant Biomass - PSF experiment 2021
+# AUTHORS: Moriah Young
+# COLLABORATORS: Kara Dobson
+# DATA INPUT: raw data - psf_biomass.csv
+# DATA OUTPUT: cleaned data - psf_biomass_L1.csv
+# PROJECT: Plant soil feedback experiment 2021
+# DATE: March 1st, 2022
+
+# Clear all existing data
+rm(list=ls())
+
+#Load packages
+library(tidyverse)
+library(janitor)
+
+# Set working directory - not working for Moriah
+L0_dir <- Sys.getenv("L0DIR_PSF")
+L1_dir <- Sys.getenv("L1DIR_PSF")
+list.files(L1_dir)
+
+# .Renviron not working for MLY; hard-coding in here
+L0_dir <- "/Volumes/GoogleDrive/Shared\ drives/SpaCE_Lab_warmXtrophic/data/PSF_greenhouse_2021/data/L0"
+L1_dir <- "/Volumes/GoogleDrive/Shared\ drives/SpaCE_Lab_warmXtrophic/data/PSF_greenhouse_2021/data/L1"
+list.files(L0_dir)
+
+### Read in csv files ###
+meta <- read.csv(file.path(L0_dir, "psf_meta_data.csv"))
+biomass <- read.csv(file.path(L0_dir, "psf_biomass_2021.csv"))
+
+names(meta)[1] <- "plant_ID" #changing column name
+biomass_1 <- merge(meta, biomass, all = TRUE) # merge meta data with biomass data
+biomass_2 <- na.omit(biomass_1)
+
+# write a new csv with the cleaned and merge data and upload to the shared google drive L1 folder
+write.csv(biomass_2, file.path(L1_dir, "./psf_biomass_2021_L1.csv"), row.names=F)
