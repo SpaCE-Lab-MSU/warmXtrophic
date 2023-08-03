cf1a67a266e67e3e3a7ec3ed5595fa80135b5e28
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-08-03 10:47:26 -0400

    fixing plant comp scripts

--- R/archive/plant_comp_clean_L1.R
+++ R/archive/plant_comp_clean_L1.R
@@ -1,11 +1,11 @@
 # TITLE:          warmXtrophic Plant composition cleanup
-# AUTHORS:        Kara Dobson, Phoebe Zarnetske, Moriah Young
+# AUTHORS:        Kara Dobson, Phoebe Zarnetske
 # COLLABORATORS:  Mark Hammond, Moriah Young
 # DATA INPUT:     Data imported as csv files from shared Google drive L0 folder
 # DATA OUTPUT:    A csv file containing plant comp data for all sites & years is uploaded
 #                 to the L1 plant comp folder
 # PROJECT:        warmXtrophic
-# DATE:           December, 2020; updated with KBS 2021 data in Nov, 2021, updated Jan. 2023
+# DATE:           December, 2020; updated with KBS 2021 data in Nov, 2021
 
 # Clear all existing data
 rm(list=ls())
@@ -22,6 +22,7 @@ source("/Users/moriahyoung/Documents/GitHub/warmXtrophic/R/L1/plant_comp_functio
 L0_dir <- Sys.getenv("L0DIR")
 L1_dir <- Sys.getenv("L1DIR")
 list.files(L1_dir)
+
 
 # Read in data (only need columns 1-7 for the umbs files)
 meta <- read.csv(file.path(L0_dir, "plot.csv"))
@@ -55,6 +56,58 @@ colnames(umbs_2016) <- sub("Percent_Cover", "Cover", colnames(umbs_2016))
 colnames(kbs_2021) <- sub("Quad_ID", "Plot", colnames(kbs_2021))
 colnames(kbs_2021) <- sub("Species_Code", "Species", colnames(kbs_2021))
 
+# Could make a function for these below - Also, how did we decide to remove these? I can't remember (Kara, Nov 2021)
+# remove a species from KBS (and not UMBS)
+kbs_2015 <- kbs_2015[!grepl("Desp",kbs_2015$Species),]
+kbs_2016 <- kbs_2016[!grepl("Desp",kbs_2016$Species),]
+kbs_2017 <- kbs_2017[!grepl("Desp",kbs_2017$Species),]
+kbs_2018 <- kbs_2018[!grepl("Desp",kbs_2018$Species),]
+kbs_2019 <- kbs_2019[!grepl("Desp",kbs_2019$Species),]
+kbs_2020 <- kbs_2020[!grepl("Desp",kbs_2020$Species),]
+kbs_2020 <- kbs_2020[!grepl("Des",kbs_2020$Species),]
+kbs_2021 <- kbs_2021[!grepl("Des",kbs_2021$Species),]
+
+# remove species from UMBS (and not KBS)
+umbs_2015 <- umbs_2015[!grepl("Vear",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Vear",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Vear",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Vear",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Vear",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Vear",umbs_2020$Species),]
+umbs_2021 <- umbs_2021[!grepl("Vear",umbs_2021$Species),]
+
+umbs_2015 <- umbs_2015[!grepl("Elre",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Elre",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Elre",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Elre",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Elre",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Elre",umbs_2020$Species),]
+umbs_2021 <- umbs_2021[!grepl("Elre",umbs_2021$Species),]
+
+umbs_2015 <- umbs_2015[!grepl("Rusp",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Rusp",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Rusp",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Rusp",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Rusp",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Rusp",umbs_2020$Species),]
+umbs_2021 <- umbs_2021[!grepl("Rusp",umbs_2021$Species),]
+
+umbs_2015 <- umbs_2015[!grepl("Ruag",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Ruag",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Ruag",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Ruag",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Ruag",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Ruag",umbs_2020$Species),]
+umbs_2021 <- umbs_2021[!grepl("Ruag",umbs_2021$Species),]
+
+umbs_2015 <- umbs_2015[!grepl("Rual",umbs_2015$Species),]
+umbs_2016 <- umbs_2016[!grepl("Rual",umbs_2016$Species),]
+umbs_2017 <- umbs_2017[!grepl("Rual",umbs_2017$Species),]
+umbs_2018 <- umbs_2018[!grepl("Rual",umbs_2018$Species),]
+umbs_2019 <- umbs_2019[!grepl("Rual",umbs_2019$Species),]
+umbs_2020 <- umbs_2020[!grepl("Rual",umbs_2020$Species),]
+umbs_2021 <- umbs_2021[!grepl("Rual",umbs_2021$Species),]
+
 # Add dataframes into a list so that needed functions can be applied 
 comp_list <- list(kbs_2015=kbs_2015, kbs_2016=kbs_2016, kbs_2017=kbs_2017, kbs_2018=kbs_2018, kbs_2019=kbs_2019, kbs_2020=kbs_2020, kbs_2021=kbs_2021,
                   umbs_2015=umbs_2015, umbs_2016=umbs_2016, umbs_2017=umbs_2017, umbs_2018=umbs_2018, umbs_2019=umbs_2019, umbs_2020=umbs_2020, umbs_2021=umbs_2021)
@@ -70,6 +123,10 @@ lapply(comp_list, spp_name)
 # Fixing site name
 comp_list <- lapply(comp_list, change_site)
 lapply(comp_list, site_name)
+
+# Remove species
+comp_list <- lapply(comp_list, remove_spp)
+lapply(comp_list, spp_name)
 
 # Merge final data
 comp_merge <- rbind(comp_list$kbs_2015, comp_list$kbs_2016, comp_list$kbs_2017, comp_list$kbs_2018, comp_list$kbs_2019, comp_list$kbs_2020, comp_list$kbs_2021,
@@ -97,13 +154,13 @@ colnames(taxon)[which(names(taxon) == "code")] <- "species"
 # Merge meta-data with plant comp data 
 # Make sure the plot IDs are the same in each, and there aren't any missing
 sort(unique(comp_merge$plot))
-plant_comp_merge <- left_join(comp_merge, meta, by = "plot")
+plant_comp_merge <- left_join(meta, comp_merge, by = "plot")
 sort(unique(plant_comp_merge$species))
 
 # taxon contains "site" which is the site where the species is found on our meta-data table, but those data exist in our plant_comp_merge dataset already.
 # Delete "site" from taxon so it doesn't accidentally get merged in.
 taxon$site<-NULL
-plant_comp_merge2 <- left_join(plant_comp_merge, taxon, by = "species")
+plant_comp_merge2 <- left_join(taxon, plant_comp_merge, by = "species")
 sort(unique(plant_comp_merge2$species))
 
 # remove species not found in our data set
@@ -111,42 +168,14 @@ plant_comp_merge3 <- plant_comp_merge2[!is.na(plant_comp_merge2$cover),]
 sort(unique(plant_comp_merge3$species))
 
 # remove unnecessary columns
+plant_comp_merge3$old_code <- NULL
+plant_comp_merge3$old_name <- NULL
 plant_comp_merge3$resolution <- NULL
-plant_comp_merge3$rhizomatous <- NULL
-plant_comp_merge3$MH_rhizomatous_suggestion <- NULL
 
 # re organize order of column names 
 plant_comp_merge3 <- plant_comp_merge3[, c("site", "plot", "species", "cover", "date", "julian", "year", "month", "treatment_key", "state",
-                                           "insecticide", "scientific_name", "common_name", "USDA_code", "LTER_code",
-                                           "origin", "group", "family", "duration", "growth_habit")]
-
-# remove "total" species bc it's not a species
-plant_comp_merge4 <- plant_comp_merge3[!(plant_comp_merge3$species=="Total"),]
-
-sort(unique(plant_comp_merge4$species))
+                           "insecticide", "scientific_name", "common_name", "USDA_code", "LTER_code",
+                           "origin", "group", "family", "duration", "growth_habit")]
 
 # Upload clean data csv to google drive without the index column
-write.csv(plant_comp_merge4, file.path(L1_dir, "plant_composition/final_plantcomp_L1.csv"), row.names=F)
-
-###############################################################################
-
-# The below code removes species that are rare in the data set or were only recorded once or twice. It also removes
-# those that aren't species, like "Bare_Ground" or "Litter"
-
-# species that were removed previously - figure out why these were removed maybe some of them shouldn't be 
-# Alpe, Apca, Brin, Juni, Plla, Romu, Stme, Umsp, Ulsp, Spsp, Acru, Oebi, Besp, Prse, Quru, Prpe, 
-# Veth, Anma, Vaan, Gnul, Trdu, Alpe, Sypi, Hipi, Pops, Acsy, Lapu, Sysa
-
-# Those that should also be removed
-# Bare_Ground, Vert_Litter, Animal_Disturbance, Litter, Brown, Moss/Lichen, Lisp (lichen), Herbicide
-# Cesp. Ruag, Soju, Cofo, Coca, Leca, Crsp, Erci, Asun, Amla (these either need to be edited or removed)
-
-# removed but maybe shouldn't be:
-# Brin,
-
-plant_comp_merge5 <- remove_spp(plant_comp_merge4)
-sort(unique(plant_comp_merge5$species))
-
-
-# Upload clean data csv to google drive without the index column - this has species that have been removed
-write.csv(plant_comp_merge5, file.path(L1_dir, "plant_composition/final_plantcomp_species_removed_L1.csv"), row.names=F)
+write.csv(plant_comp_merge3, file.path(L1_dir, "plant_composition/final_plantcomp_L1.csv"))
